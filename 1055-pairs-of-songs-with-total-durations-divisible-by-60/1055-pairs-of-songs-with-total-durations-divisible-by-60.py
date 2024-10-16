98154c74e69498b72ce12889659673c82cbf6101
Donggyun Nam <46705205+ndk6879@users.noreply.github.com>
2024-10-16 16:47:36 +0900

    Time: 220 ms (81.62%), Space: 21.2 MB (37.02%) - LeetHub

--- 1055-pairs-of-songs-with-total-durations-divisible-by-60/1055-pairs-of-songs-with-total-durations-divisible-by-60.py
+++ 1055-pairs-of-songs-with-total-durations-divisible-by-60/1055-pairs-of-songs-with-total-durations-divisible-by-60.py
@@ -9,14 +9,13 @@ class Solution:
         two pointer, binart search, sliding window
         '''
         
-        remainder = [0] * 60  # 각 노래의 시간을 60으로 나눈 나머지를 저장하는 배열
-        count = 0  # 결과로 반환할 쌍의 개수
+        remainder = 60 * [0]
+        ans = 0
 
         for t in time:
-            mod = t % 60  # 노래 시간을 60으로 나눈 나머지
-            complement = (60 - mod) % 60  # 60과 더해서 60의 배수가 되는 나머지 값을 계산
+            mod = t % 60
+            complement = (60 - mod) % 60
             
-            count += remainder[complement]  # complement가 존재하면 그 수만큼 쌍을 더함
-            remainder[mod] += 1  # 현재 노래의 나머지를 카운트 배열에 추가
-        
-        return count  # 최종 쌍의 개수를 반환
+            ans += remainder[complement]
+            remainder[mod] += 1
+        return ans
