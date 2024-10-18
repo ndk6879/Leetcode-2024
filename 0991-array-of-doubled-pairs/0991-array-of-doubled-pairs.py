d4f777bccab13c2b3cd9dff40ae408725ba63449
Donggyun Nam <46705205+ndk6879@users.noreply.github.com>
2024-10-18 19:11:42 +0900

    Time: 20 ms (100%), Space: 18.8 MB (96.57%) - LeetHub

--- 0991-array-of-doubled-pairs/0991-array-of-doubled-pairs.py
+++ 0991-array-of-doubled-pairs/0991-array-of-doubled-pairs.py
@@ -1,11 +1,9 @@
+from collections import Counter
 class Solution:
     def canReorderDoubled(self, arr: List[int]) -> bool:
-        from collections import Counter
         counter = Counter(arr)
-        
-        for num in sorted(counter, key=abs):  # 절대값 기준으로 작은 수부터 확인
-            if counter[num] > counter[2 * num]:
+        for num in sorted(counter, key = abs):
+            if counter[num] > counter[num*2]:
                 return False
-            counter[2 * num] -= counter[num]
-        
+            counter[num*2] -= counter[num]
         return True
