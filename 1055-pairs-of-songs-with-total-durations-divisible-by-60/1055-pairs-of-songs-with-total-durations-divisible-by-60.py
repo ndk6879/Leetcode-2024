e7f3b4f528ea7da8fc08afaae43c3d81904bedcb
Donggyun Nam <46705205+ndk6879@users.noreply.github.com>
2024-10-21 16:30:38 +0900

    Time: 16 ms (99.53%), Space: 20 MB (99.61%) - LeetHub

--- 1055-pairs-of-songs-with-total-durations-divisible-by-60/1055-pairs-of-songs-with-total-durations-divisible-by-60.py
+++ 1055-pairs-of-songs-with-total-durations-divisible-by-60/1055-pairs-of-songs-with-total-durations-divisible-by-60.py
@@ -1,21 +1,12 @@
+from collections import Counter
+
 class Solution:
     def numPairsDivisibleBy60(self, time: List[int]) -> int:
-
-        '''
-        1. get all combinations of times
-        2. check if divisible by 60
-
-        or 
-        two pointer, binart search, sliding window
-        '''
-        
-        remainder = 60 * [0]
+        remainder = [0] * 60
         ans = 0
-
         for t in time:
             mod = t % 60
             complement = (60 - mod) % 60
-            
             ans += remainder[complement]
             remainder[mod] += 1
         return ans
