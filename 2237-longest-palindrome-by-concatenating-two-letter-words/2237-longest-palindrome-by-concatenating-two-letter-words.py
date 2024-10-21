7c9a6473c6bcf4cfae3bbb9b0f683345b858bfa9
Donggyun Nam <46705205+ndk6879@users.noreply.github.com>
2024-10-21 17:00:15 +0900

    Time: 43 ms (99.65%), Space: 34.7 MB (97.82%) - LeetHub

--- 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.py
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.py
@@ -1,23 +1,22 @@
-from collections import Counter
-
 class Solution:
     def longestPalindrome(self, words: List[str]) -> int:
+        from collections import Counter
 
         counter = Counter(words)
         ans = 0
         odd = False
-
+        
         for word, count in counter.items():
-            reverse_word = word[::-1]
-            if word == reverse_word:  # 같은 두 글자로 이루어진 단어 (예: "cc")
-                ans += (count // 2) * 4  # 쌍을 만들 수 있으면 그 길이를 추가
+            reversedWord = word[::-1]
+            if word == reversedWord:
+                ans += (count//2) * 4
                 if count % 2 == 1:
-                    odd = True  # 홀수 개가 있다면 중앙에 넣을 수 있음
-            elif word > reverse_word:  # 같은 단어가 아니고, 사전순으로 한 번만 처리
-                pairs = min(count, counter[reverse_word])
-                ans += pairs * 4  # 쌍으로 팰린드롬의 양 끝을 채움
+                    print('odd')
+                    odd = True
 
+            elif word > reversedWord:
+                print('dsa')
+                ans += min(count, counter[reversedWord]) * 4
         if odd:
-            ans += 2  # 중앙에 넣을 단어가 있으면 추가
-
+            ans += 2
         return ans
