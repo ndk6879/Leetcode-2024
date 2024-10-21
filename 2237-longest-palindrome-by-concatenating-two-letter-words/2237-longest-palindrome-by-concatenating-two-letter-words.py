1da6217c28b583c6b7fbdd4ab398cfce9a1afc57
Donggyun Nam <46705205+ndk6879@users.noreply.github.com>
2024-10-21 16:50:08 +0900

    Time: 52 ms (99.12%), Space: 34.6 MB (99.29%) - LeetHub

--- 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.py
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.py
@@ -13,7 +13,7 @@ class Solution:
                 ans += (count // 2) * 4  # 쌍을 만들 수 있으면 그 길이를 추가
                 if count % 2 == 1:
                     odd = True  # 홀수 개가 있다면 중앙에 넣을 수 있음
-            elif word < reverse_word:  # 같은 단어가 아니고, 사전순으로 한 번만 처리
+            elif word > reverse_word:  # 같은 단어가 아니고, 사전순으로 한 번만 처리
                 pairs = min(count, counter[reverse_word])
                 ans += pairs * 4  # 쌍으로 팰린드롬의 양 끝을 채움
 
