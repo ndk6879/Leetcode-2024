b29c704c6321a20aa32db094242fe98cfdaab705
Donggyun Nam <46705205+ndk6879@users.noreply.github.com>
2024-10-23 15:02:57 +0900

    Time: 35 ms (86.63%), Space: 17 MB (14.71%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -1,19 +1,3 @@
-'''
-2/9
-bfs를 써서 푸는데 dictionary를 써서 neighbor를 저장.
-d[current].neighbors.append(d[neighbor])를 해줘서 이웃이 뭔지 알게해줌.
-time: O(V + E)
-memory: O(V) for hashmap
-'''
-
-
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val = 0, neighbors = None):
-        self.val = val
-        self.neighbors = neighbors if neighbors is not None else []
-"""
 
 class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
