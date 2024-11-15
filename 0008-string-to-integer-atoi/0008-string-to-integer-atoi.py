1445c9bf167e8cf4840689a2d2297fdb59c73298
Donggyun Nam <46705205+ndk6879@users.noreply.github.com>
2024-11-14 23:13:16 -0500

    Time: 40 ms (5.82%), Space: 13.8 MB (100%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,30 +1,32 @@
 class Solution:
-    def myAtoi(self, s: str) -> int:
-        # 초기 설정
-        i, n = 0, len(s)
+    def myAtoi(self, strs):
+        min_value = (-2)**31 
+        max_value = (2)**31 - 1
+
+        strs = (strs.strip())
+        print('strs:',strs)
+        if (strs).isdigit():
+            return (max(min_value, min(int(strs), max_value)))
+
+        i = 0
         sign = 1
-        result = 0
-        
-        # 1. 공백 제거
-        while i < n and s[i] == ' ':
-            i += 1
+        ans = ''
+        non_int = 0
+        while (i < len(strs)):
+            if i ==0 and strs[i] == '-':
+                sign = -1    
 
-        # 2. 부호 확인
-        if i < n and s[i] in ('+', '-'):
-            sign = -1 if s[i] == '-' else 1
-            i += 1
+            elif  i ==0 and strs[i] == '+':
+                sign = +1
+                
+            elif strs[i].isdigit():
+                ans += str(strs[i])
 
-        # 3. 숫자 추출
-        while i < n and s[i].isdigit():
-            result = result * 10 + int(s[i])  # 현재 숫자를 결과에 추가
-            i += 1
+            else:
+                break
 
-        # 4. 정수 범위 제한
-        result *= sign
-        int_min, int_max = -2**31, 2**31 - 1
-        if result < int_min:
-            return int_min
-        if result > int_max:
-            return int_max
+            i+= 1
+        if ans == '':
+            return (0)
+        return (max(min_value, min(sign * int(ans), max_value)))
 
-        return result
