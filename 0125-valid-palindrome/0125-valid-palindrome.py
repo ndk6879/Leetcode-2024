a133dd54ad043f280c32ceffe92866a394b99308
Donggyun Nam <46705205+ndk6879@users.noreply.github.com>
2024-11-17 00:33:14 -0500

    Time: 11 ms (37.36%), Space: 16.9 MB (87.3%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,12 +1,22 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        newS = ""
-        
-        for i in s:
-            if i.isalpha():
-                newS += i.lower()
+        l, r = 0, len(s)-1
+
+        while(l < r):
+            while l<r and not s[l].isalpha() and not s[l].isdigit():
+                l += 1
+
+            while l<r and not s[r].isalpha() and not s[r].isdigit():
+                r -= 1
             
-            elif i.isdigit():
-                newS += i
-        print('newS:',newS)
-        return newS == newS[::-1]
+            if s[l].lower() == s[r].lower():
+                l += 1
+                r -= 1
+
+            else:
+                print('s[l]:',s[l])
+                print('s[r]:',s[r])
+                return False
+
+            
+        return True
