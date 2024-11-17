cb27a8372dde44136e850e48f3dfa1c54cd7d450
Donggyun Nam <46705205+ndk6879@users.noreply.github.com>
2024-11-17 02:31:37 -0500

    Time: 0 ms (100%), Space: 16.9 MB (16.22%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -1,35 +1,45 @@
 class MyStack:
 
     def __init__(self):
-        self.q1 = deque()
-        self.q2 = deque()
+        self.queue1 = deque([])
+        self.queue2 = deque([])
+        
 
     def push(self, x: int) -> None:
-        self.q1.append(x)
+        self.queue1.append(x)
+        
 
     def pop(self) -> int:
-        while len(self.q1) > 1:
-            self.q2.append(self.q1.popleft())
+        while(len(self.queue1) > 1):
+            self.queue2.append(self.queue1.popleft())
+            
+        ans = self.queue1.popleft()
+        print('POPPOP self.queue1:',self.queue2)
+        self.queue1, self.queue2 = self.queue2, self.queue1
+        print('POP self.queue1:',self.queue1)
+
+        return ans
         
-        popped_element = self.q1.popleft()
-        
-        # Swap q1 and q2
-        self.q1, self.q2 = self.q2, self.q1
-        
-        return popped_element
 
     def top(self) -> int:
-        while len(self.q1) > 1:
-            self.q2.append(self.q1.popleft())
+        while(len(self.queue1) > 1):
+            self.queue2.append(self.queue1.popleft())
+        ans = self.queue1[0]
+        self.queue2.append(self.queue1.popleft())
+        # queue1과 queue2 교환
+        self.queue1, self.queue2 = self.queue2, self.queue1
+        return ans
         
-        top_element = self.q1[0]
-        
-        self.q2.append(self.q1.popleft())
-        
-        # Swap q1 and q2
-        self.q1, self.q2 = self.q2, self.q1
-        
-        return top_element
 
     def empty(self) -> bool:
-        return len(self.q1) == 0
+        print('EMPTY self.queue1:',self.queue1)
+        return len(self.queue1) == 0
+        
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
