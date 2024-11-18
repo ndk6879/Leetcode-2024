a38165634a33e42729bfd738a5abdcd3dfd3fe6c
Donggyun Nam <46705205+ndk6879@users.noreply.github.com>
2024-11-18 10:45:27 -0500

    Time: 0 ms (100%), Space: 17 MB (56.69%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,23 +1,24 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l, r = 0, len(nums) - 1
-        ans = nums[0]
-
-        while(l<=r):
-            mid = (l + r) // 2
-
+        left, right = 0, len(nums) - 1
+    
+        while left <= right:
+            mid = (left + right) // 2
+            
             if nums[mid] == target:
                 return mid
             
-            if nums[l] <= nums[mid]:
-                if nums[l] > target or nums[mid] < target:
-                    l = mid + 1
-                else: 
-                    r = mid - 1
-
-            elif nums[mid] <= nums[r]:
-                if nums[r] < target or nums[mid] > target:
-                    r = mid - 1
+            # 왼쪽 절반이 정렬된 경우
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
                 else:
-                    l = mid + 1
+                    left = mid + 1
+            # 오른쪽 절반이 정렬된 경우
+            else:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        
         return -1
