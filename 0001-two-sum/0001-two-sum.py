1577e3f633730842f05a9835fc8c7cbb1e7f0ef9
Donggyun Nam <46705205+ndk6879@users.noreply.github.com>
2024-11-18 08:30:06 -0500

    Time: 3 ms (53.98%), Space: 17.9 MB (31.29%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         adict = {}
-        for ind,num in enumerate(nums):
+        for ind, num in enumerate(nums):
             if target - num not in adict:
                 adict[num] = ind
-
-            else:
-                return [adict[target - num],ind]
-
+            elif target - num in adict:
+                return [ind, adict[target - num]]
