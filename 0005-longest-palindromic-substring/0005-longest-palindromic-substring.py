0a45bc20d9856ba261511a04c62452a047299e85
Donggyun Nam <46705205+ndk6879@users.noreply.github.com>
2024-11-19 01:19:51 -0500

    Time: 286 ms (57.21%), Space: 16.6 MB (64.25%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,27 +1,24 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        # for loop에서 각각의 letter go through할때, each letter에서 outward로 체크하는거임. 이것만 기억하셈
-        
         ans = ''
         ansLen = 0
-
-        for i in range(len(s)):
-            
-            l,r = i, i 
-            while(0 <= l and r < len(s) and s[l] == s[r]):
-                if ansLen < (r+1-l):
-                    ans = s[l:r+1]
-                    ansLen = r + 1 - l
-                l -= 1
-                r += 1
-
-            l, r = i, i + 1
-                
-            while(0 <= l and r < len(s) and s[l] == s[r]):
-                if ansLen < (r+1-l):
-                    ans = s[l:r+1]
-                    ansLen = r + 1 - l
-                l -= 1
-                r += 1
         
+        for i in range(len(s)):
+            l, r = i, i
+
+            while(l>=0 and r < len(s) and s[l] == s[r]):
+                if ansLen < (r+1-l):
+                    ans = s[l:r+1]
+                    ansLen = (r+1-l)
+                l -= 1
+                r += 1
+                
+            l, r = i, i + 1
+
+            while(l>=0 and r < len(s) and s[l] == s[r]):
+                if ansLen < (r+1-l):
+                    ans = s[l:r+1]
+                    ansLen = (r+1-l)
+                l -= 1
+                r += 1
         return ans
