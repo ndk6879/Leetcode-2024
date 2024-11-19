e726b0acbd7200c3b0f6c588b94a62c3dd0d5200
Donggyun Nam <46705205+ndk6879@users.noreply.github.com>
2024-11-19 12:04:08 -0500

    Time: 16 ms (27.51%), Space: 16.8 MB (50.13%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,14 +1,13 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        if numRows == 1: return s 
-        ans = ''
+        ans = ""
+        
+        if numRows == 1: return s
         
         for r in range(numRows):
-            interval = 2*(numRows-1) #0 4 , 1/5
-            for i in range(r,len(s), interval):
+            intervals = (numRows - 1) * 2
+            for i in range(r,len(s),intervals):
                 ans += s[i]
-                if (r > 0 and r < numRows -1 and 
-                (i + interval - 2*r) < len(s)):
-                    ans += s[i + interval - 2*r]
-
+                if (0 < r and r < (numRows)-1 and (i+intervals-2*r) < len(s)):
+                    ans += s[i+intervals-2*r]
         return ans
