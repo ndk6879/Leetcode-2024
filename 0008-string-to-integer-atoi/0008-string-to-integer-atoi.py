b0c88221691d6846f53ecbbdd499e20d107f22be
Donggyun Nam <46705205+ndk6879@users.noreply.github.com>
2024-11-19 03:56:08 -0500

    Time: 3 ms (40.51%), Space: 16.8 MB (18.04%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,31 +1,18 @@
 class Solution:
-    def myAtoi(self, strs):
-        minValue = -2**(31)
-        maxValue = 2**(31)-1
-
-        s = strs.strip()
-        
-
-        i = 0
+    def myAtoi(self, s: str) -> int:
+        ans = 0
         sign = 1
-        ans = ''
-        while(i<len(s)):
-            if s[i] == '+' and i == 0:
+        s = s.strip()
+        minAns = - 2 ** 31
+        maxAns = (2**31) - 1
+        for i in range(len(s)) :
+            
+            if i == 0 and s[i] == '+':
                 sign = 1
-
-            elif s[i] == '-' and i == 0:
+            elif i == 0 and s[i] == '-':
                 sign = -1
-
             elif s[i].isdigit():
-                ans += s[i] 
-            
-            else:
-                break
-            i += 1
-            
+                ans = ans * 10 + ord(s[i]) - ord('0')
 
-        total = 0
-        for i in ans:
-            total = total*10 + ord(i) - ord('0')
-
-        return max(minValue, min(sign*total,maxValue) )
+            elif not s[i].isdigit(): break
+        return max(minAns,min(sign*ans, maxAns))
