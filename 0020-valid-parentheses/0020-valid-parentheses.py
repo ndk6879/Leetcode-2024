6fa70a2e22c4550032c45a85dbe1be94d1d3434f
Donggyun Nam <46705205+ndk6879@users.noreply.github.com>
2024-11-19 04:51:35 -0500

    Time: 0 ms (100%), Space: 16.7 MB (49.04%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,18 +1,12 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-
-        opening = {')':'(',
-        '}':'{',
-        ']':'['}
-        
+        opening = {']':'[','}':'{',')':'('}
+        openingList = ['[','(','{']
         for i in s:
-            if i in opening.values():
+            if i in openingList:
                 stack.append(i)
-
             else:
-                if stack == []: return False
-                if not opening[i] == stack.pop(): return False
-                
+                if stack == [] or not opening[i] == stack.pop(): return False
+
         return stack == []
-        
