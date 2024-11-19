a61574a623aadde666b285fe45a93ee5dc45bb85
Donggyun Nam <46705205+ndk6879@users.noreply.github.com>
2024-11-19 09:30:38 -0500

    Time: 100 ms (93.61%), Space: 78.5 MB (17.8%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,23 +1,29 @@
 from collections import OrderedDict
-
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.cache = OrderedDict()
-        self.capacity = capacity
+        self.cap = capacity
+        
 
     def get(self, key: int) -> int:
         if key in self.cache:
-            # 가장 최근에 사용한 항목으로 갱신
             self.cache.move_to_end(key)
             return self.cache[key]
         return -1
+        
 
     def put(self, key: int, value: int) -> None:
+        
         if key in self.cache:
-            # 기존 키 갱신
             self.cache.move_to_end(key)
         self.cache[key] = value
-        if len(self.cache) > self.capacity:
-            # 가장 오래된 항목 제거
+        if self.cap < len(self.cache):
             self.cache.popitem(last=False)
+        
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
