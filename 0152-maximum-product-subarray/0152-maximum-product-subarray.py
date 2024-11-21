507556060aac3964ce65411d90008d378969a8ae
Donggyun Nam <46705205+ndk6879@users.noreply.github.com>
2024-11-21 11:30:12 -0500

    Time: 5 ms (94.59%), Space: 17.4 MB (13.04%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,15 +1,13 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        '''
-        max가 될 수 있는 경우는 1. num 그자체 2. 아니면 curMax * num 3. 아니면 num * Min
-        min이 될수있는 경우도 똑같음. 근데 max가 업데이트 되서 tmp를 위에서 생성하는거임
-        
-        '''
-        ans = max(nums)
-        curMax, curMin = 1,1
+        if len(nums) == 1: return nums[-1]
+        arr = [1] * len(nums)
+        valMax = 0
+        valMin = 0
+        ans = 0
         for num in nums:
-            tmp = curMax*num
-            curMax = max(num, curMax*num,curMin*num) 
-            curMin = min(num, tmp,curMin*num)
-            ans = max(ans, curMax)
+            tmp = valMax
+            valMax = max(num, valMax * num, valMin*num)
+            valMin = min(num, valMin * num, tmp*num)
+            ans = max(ans,valMax)
         return ans
