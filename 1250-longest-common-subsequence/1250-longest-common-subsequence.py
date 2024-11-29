3b2fb8ae781e2579382bb09fb7ae3dcd2eedb648
Donggyun Nam <46705205+ndk6879@users.noreply.github.com>
2024-11-29 10:44:10 -0500

    Time: 575 ms (35.64%), Space: 43.4 MB (22.51%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.py
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -6,13 +6,13 @@ class Solution:
             for j in range(len(text2)+1):
                 tmp.append(0)
             arr.append(tmp)
-        
+
         for i in range(len(text1)-1,-1,-1):
             for j in range(len(text2)-1,-1,-1):
                 if text1[i] == text2[j]:
-                    arr[i][j] = arr[i+1][j+1] + 1
+                    arr[i][j] = arr[i+1][j+1]+1
                 else:
-                    arr[i][j] = max(arr[i+1][j],arr[i][j+1])
+                    arr[i][j] = max(arr[i][j+1],arr[i+1][j])
         for i in arr:
             print(i)
         return arr[0][0]
