27b1a0787d2d15a9bd47101c05c27b966ab5827f
Donggyun Nam <46705205+ndk6879@users.noreply.github.com>
2024-12-01 02:28:16 -0500

    Time: 43 ms (37.02%), Space: 17.3 MB (7.45%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.py
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -1,11 +1,12 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        dp = [0] * (target + 1)
-        dp[0] = 1  # 합이 0인 경우 방법의 수는 1
-
-        for i in range(1, target + 1):
+        arr = [0] * (target + 1)
+        arr[0] = 1
+        for i in range(1, len(arr)):
             for num in nums:
-                if i >= num:  # num을 사용 가능할 때
-                    dp[i] += dp[i - num]
+                if i - num >= 0:
+                    arr[i] += arr[i-num]
+        
+        print('arr:',arr)
 
-        return dp[target]
+        return arr[target]
