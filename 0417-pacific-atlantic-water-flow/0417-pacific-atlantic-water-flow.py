77c4075729b48de66c4cf9205836b6fbd07135d4
Donggyun Nam <46705205+ndk6879@users.noreply.github.com>
2024-12-03 12:31:20 -0500

    Time: 39 ms (39.03%), Space: 18.6 MB (17.21%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,40 +1,39 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        
-        '''
-        #1. create set for pacific ocean and atlantic ocean
-        #2. add each proper coordinate in set
-        #3. if a coordinate is in both set, add it as answer
-        '''
-        pacificSet = set()
-        atlanticSet = set()
+        pac, atl = set(), set()
 
         def dfs(i,j,visit,prevheight):
             if (i < 0 or 
             i >= len(heights) or
             j < 0 or 
             j >= len(heights[0]) or
-            (i,j) in visit or
-            heights[i][j] < prevheight):
-                return
+            ((i,j)) in visit or
+            heights[i][j] < prevheight
+            ): return
 
             visit.add((i,j))
             dfs(i-1,j,visit,heights[i][j])
-            dfs(i,j+1,visit,heights[i][j])
             dfs(i+1,j,visit,heights[i][j])
             dfs(i,j-1,visit,heights[i][j])
-        
+            dfs(i,j+1,visit,heights[i][j])
+            return True
+
+
         for i in range(len(heights)):
-            dfs(i,0,pacificSet,heights[i][0]) #첫번째 column
-            dfs(i,len(heights[0])-1,atlanticSet,heights[i][len(heights[0])-1]) #마지막 column
+            dfs(i,0,pac,heights[i][0])
+            dfs(i,len(heights[0])-1,atl,heights[i][-1])
 
         for i in range(len(heights[0])):
-            dfs(0,i,pacificSet,heights[0][i]) #첫번째 row
-            dfs(len(heights)-1,i,atlanticSet,heights[len(heights)-1][i]) #마지막 row
+            dfs(0,i,pac,heights[0][i])
+            dfs(len(heights)-1,i,atl,heights[-1][i])
 
+        
         ans = []
-        for i in range(len(heights)):
-            for j in range(len(heights[0])):
-                if (i,j) in atlanticSet and (i,j) in pacificSet:
-                    ans.append([i,j])
+        for i in pac:
+            if i in atl:
+                ans.append(i)
+        print('ans:',ans)
+
         return ans
+
+        
