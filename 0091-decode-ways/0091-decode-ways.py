7e4a90738fad1cf11ccee52095a1af05f9e45ae2
Donggyun Nam <46705205+ndk6879@users.noreply.github.com>
2024-12-04 12:12:00 -0500

    Time: 2 ms (34.73%), Space: 17.2 MB (10.6%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,20 +1,18 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        if not s or s[0] == '0':
+        arr = [0] * (len(s)+1)
+        if s[0] == '0':
             return 0
-        
-        n = len(s)
-        dp = [0] * (n + 1)
-        dp[0] = 1
-        dp[1] = 1
-        
-        for i in range(2, n + 1):
-            # 한 자리 수 고려
-            if s[i-1] != '0':
-                dp[i] += dp[i-1]
-            # 두 자리 수 고려
-            two_digit = int(s[i-2:i])
-            if 10 <= two_digit <= 26:
-                dp[i] += dp[i-2]
-        
-        return dp[n]
+
+        arr[0] = 1
+        arr[1] = 1
+
+        #226, [1,1,0,0]
+        for i in range(2,len(s)+1):
+            if '0' < s[i-1] <= '9':
+                arr[i] += arr[i-1]
+
+            if '10' <= s[i-2:i] <= '26':
+                arr[i] += arr[i-2]
+            
+        return arr[-1]
