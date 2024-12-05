3262a53d80c20cc68dbc8c8119f24c9d0b025fab
Donggyun Nam <46705205+ndk6879@users.noreply.github.com>
2024-12-04 23:26:55 -0500

    Time: 34 ms (90.83%), Space: 17.8 MB (7.27%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -1,18 +1,28 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
 
+from typing import Optional
 class Solution:
-    def cloneGraph(self, node: 'Node') -> 'Node':
-        if not node:
-            return 
-        
-        d = {node:Node(node.val)}
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        from collections import deque
+        if not node: return 
+        d = {node : Node(node.val)}
+
         queue = deque([node])
-        
+
         while queue:
-            for i in range(len(queue)):
-                current = queue.popleft()
-                for neighbor in current.neighbors:
-                    if neighbor not in d:
-                        d[neighbor] = Node(neighbor.val)
-                        queue.append(neighbor)
-                    d[current].neighbors.append(d[neighbor])
+            cur = queue.popleft()
+            for n in cur.neighbors:
+                if n not in d:
+                    d[n] = Node(n.val)
+                    queue.append(n)
+                d[cur].neighbors.append(d[n])
+                
+                
+                
         return d[node]
