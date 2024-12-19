475db9535439656c886e39663b0fe9bb1b2ff23c
Donggyun Nam <46705205+ndk6879@users.noreply.github.com>
2024-12-19 00:50:48 -0500

    Time: 44 ms (16.08%), Space: 17.9 MB (6.16%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,10 +1,14 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        hashMap = {}
+        l = 0
         ans = 0
-        string = ''
-        for i in s:
-            string += i
-            while len(string) != len(set(string)): #만약 새로 추가된 i가 이미 string에 있다면
-                string = string[1:] #string의 크기를 줄임. 언제까지? string의 모든문자가 unique할때까지 (while loop을 씀으로서)
-            ans = max(ans, len(string))
+        for r in range(len(s)):
+            if s[r] in hashMap:
+                l = max(hashMap[s[r]]+1,l)
+
+            hashMap[s[r]] = r
+
+            ans = max(ans, r + 1 - l)
+            print('ans:',ans)
         return ans
