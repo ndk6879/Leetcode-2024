3f1780a5f592ddf888dc8cd70b64f8ae3f0a416d
Donggyun Nam <46705205+ndk6879@users.noreply.github.com>
2024-12-19 20:17:59 -0500

    Time: 79 ms (26.25%), Space: 18 MB (7.95%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -2,22 +2,19 @@ from collections import Counter
 
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        # 필요한 문자 카운트
         s1Cnt = Counter(s1)
-        # 현재 슬라이딩 윈도우의 문자 카운트
         windowCnt = Counter()
+        l = 0  # 윈도우의 시작점
         
-        n, m = len(s1), len(s2)
-        
-        for i in range(m):
-            # 윈도우에 새로운 문자 추가
-            windowCnt[s2[i]] += 1
+        for r in range(len(s2)):  # r은 윈도우의 끝점
+            # 윈도우에 새 문자 추가
+            windowCnt[s2[r]] += 1
             
-            # 윈도우 크기가 s1과 같아지면 왼쪽 문자 제거
-            if i >= n:
-                windowCnt[s2[i - n]] -= 1
+            while r - l + 1 > len(s1):
+                windowCnt[s2[l]] -= 1
+                l += 1  # 윈도우의 시작점을 한 칸 오른쪽으로 이동
             
-            # 현재 윈도우와 s1의 문자 카운트가 같으면 True 반환
+            # 윈도우와 s1의 문자 카운트 비교
             if windowCnt == s1Cnt:
                 return True
         
