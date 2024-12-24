3c0798d88e1100b02e97d7017d143ccd5b835b20
Donggyun Nam <46705205+ndk6879@users.noreply.github.com>
2024-12-24 00:41:29 -0500

    Time: 27 ms (48.96%), Space: 18.1 MB (6.77%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -9,7 +9,7 @@ class Solution:
         for r in range(len(s2)):
             s2Counter[s2[r]] = s2Counter.get(s2[r],0) + 1
 
-            if r+1-l > len(s1):
+            if r + 1 -l > len(s1):
                 s2Counter[s2[l]] -= 1
                 
                 if s2Counter[s2[l]] == 0:
