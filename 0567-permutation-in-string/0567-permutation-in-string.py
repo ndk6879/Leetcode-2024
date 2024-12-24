b5dc7e5144421f6f309e52fa016be39134bb06f9
Donggyun Nam <46705205+ndk6879@users.noreply.github.com>
2024-12-24 00:40:57 -0500

    Time: 23 ms (55.69%), Space: 17.9 MB (6.77%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,32 +1,23 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        s1Count = {}
+        from collections import Counter
 
-        for i in s1:
-            if i not in s1Count:
-                s1Count[i] = 1
-            else:
-                s1Count[i] += 1
+        s1Counter = Counter(s1)
 
-        s2Count = {}
+        s2Counter = {}
         l = 0
-        for i in range(len(s2)):
+        for r in range(len(s2)):
+            s2Counter[s2[r]] = s2Counter.get(s2[r],0) + 1
 
-            if s2[i] not in s2Count:
-                s2Count[s2[i]] = 1
-
-            else:
-                s2Count[s2[i]] += 1
-
-            print('s2Count:',s2Count)
-
-            while i + 1 - l > len(s1):
-                s2Count[s2[l]] -= 1
-                if s2Count[s2[l]] == 0:
-                    del s2Count[s2[l]]
+            if r+1-l > len(s1):
+                s2Counter[s2[l]] -= 1
+                
+                if s2Counter[s2[l]] == 0:
+                    del s2Counter[s2[l]] 
                 l += 1
 
-            if s2Count == s1Count:
+
+            if s2Counter == s1Counter:
                 return True
 
         return False
