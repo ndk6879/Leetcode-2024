2f5edb46162c1370975453b7db54d37173592538
Donggyun Nam <46705205+ndk6879@users.noreply.github.com>
2024-12-24 01:54:51 -0500

    Time: 0 ms (100%), Space: 18.6 MB (7.28%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -5,7 +5,6 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-
         dummy = None
 
         while head:
@@ -13,5 +12,5 @@ class Solution:
             head.next = dummy
             dummy = head
             head = tmp
-        return dummy
         
+        return dummy
