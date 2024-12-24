c589c55a5f4173e4a3a5316cb7a82a2ac34e20cb
Donggyun Nam <46705205+ndk6879@users.noreply.github.com>
2024-12-24 00:42:03 -0500

    Time: 29 ms (45.07%), Space: 18.1 MB (6.77%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -9,7 +9,7 @@ class Solution:
         for r in range(len(s2)):
             s2Counter[s2[r]] = s2Counter.get(s2[r],0) + 1
 
-            if r + 1 -l > len(s1):
+            if r + 1 -l > len(s1): #len(s1Counter)나 len(s2Counter)로 비교하면 개수가 틀려짐. 왜냐히면 중복된건 개수가 안늘어나니까
                 s2Counter[s2[l]] -= 1
                 
                 if s2Counter[s2[l]] == 0:
