c31eb4a3cc1088a53c18d815b6c7e130f0746537
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-01-17 12:31:06 -0500

    Time: 45 ms (57.34%), Space: 21.5 MB (28.67%) - LeetHub

--- 0817-design-hashmap/0817-design-hashmap.py
+++ 0817-design-hashmap/0817-design-hashmap.py
@@ -1,21 +1,47 @@
+class ListNode:
+    def __init__(self, key = None, value = None, next = None):
+        self.key = key
+        self.value = value
+        self.next = next
+
 class MyHashMap:
 
     def __init__(self):
-        self.map = {}
+        self.map = [ListNode() for i in range(1000)]
+
+
+    def hash(self,key):
+        return key % len(self.map)
         
 
     def put(self, key: int, value: int) -> None:
-        self.map[key] = value
+        index = self.hash(key)
+        cur = self.map[index]
+        while cur.next:
+            if cur.next.key == key:
+                cur.next.value = value
+                return
+            cur = cur.next
         
+        cur.next = ListNode(key, value)
 
     def get(self, key: int) -> int:
-        if key in self.map:
-            return self.map[key]
+        index = key % len(self.map)
+        cur = self.map[index]
+        while cur.next:
+            if cur.next.key == key:
+                return cur.next.value
+            cur = cur.next
         return -1
 
     def remove(self, key: int) -> None:
-        if key in self.map:
-            del self.map[key]
+        index = key % 1000
+        cur = self.map[index]
+        while cur and cur.next:
+            if cur.next.key == key:
+                cur.next = cur.next.next
+                return
+            cur = cur.next
         
 
 
