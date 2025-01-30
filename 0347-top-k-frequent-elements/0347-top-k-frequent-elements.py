a62ec51c17d1f9ed7d52f14a502cce40e0de397f
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-01-30 00:41:37 -0500

    Time: 12 ms (17.03%), Space: 24.3 MB (9.79%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,20 +1,25 @@
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        from collections import Counter
-        adict = Counter(nums)
-        print('adict:',adict)
+        adict = {}
+        for num in nums:
+            if num not in adict:
+                adict[num] = 0
+            else:
+                adict[num] += 1
+        
+        arr = [[] for _ in range(len(nums))]
 
-        arr = [[] for _ in range(len(nums)+1)]
         for num in adict:
-            index = adict[num]
-            arr[index].append(num)
+            occurrence = adict[num]
+            arr[occurrence].append(num)
 
-        print('arr:',arr)
 
         ans = []
         for i in range(len(arr)-1,-1,-1):
-            for num in arr[i]:
-                ans.append(num)
-                k -= 1
-                if k == 0:
-                    return ans
+            if len(arr[i]) > 0:
+                while arr[i]:
+                    cur = arr[i].pop()
+                    ans.append(cur)
+                    k -= 1
+                    if k == 0: return ans
