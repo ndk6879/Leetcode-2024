e071612fb0e81537dda4baa43b31f7fea6b7d47c
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-02-04 06:29:49 -0500

    Time: 31 ms (43.75%), Space: 18.1 MB (15.82%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,23 +1,22 @@
+from collections import Counter
+
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        from collections import Counter
-
         s1Counter = Counter(s1)
 
-        s2Counter = {}
         l = 0
+        s2Counter = {}
         for r in range(len(s2)):
             s2Counter[s2[r]] = s2Counter.get(s2[r],0) + 1
 
-            if r + 1 -l > len(s1): #len(s1Counter)나 len(s2Counter)로 비교하면 개수가 틀려짐. 왜냐히면 중복된건 개수가 안늘어나니까
+            if r + 1 - l > len(s1):
                 s2Counter[s2[l]] -= 1
-                
+
                 if s2Counter[s2[l]] == 0:
-                    del s2Counter[s2[l]] 
+                    del s2Counter[s2[l]]
                 l += 1
 
-
-            if s2Counter == s1Counter:
+            if s1Counter == s2Counter:
                 return True
 
         return False
