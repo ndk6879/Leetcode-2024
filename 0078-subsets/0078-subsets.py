ed948ee738a4c24a1027aced4c4331f3d8f402f7
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-02-09 22:23:25 -0500

    Time: 0 ms (100%), Space: 18.1 MB (19.26%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,10 +1,20 @@
-from itertools import combinations
-
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        ans = [[]]
-        for i in range(len(nums)):
-            for j in (list(combinations(nums,i+1))):
-                ans.append(j)
 
+        ans = []
+        cur = []
+
+        def dfs(i):
+            if i >= len(nums):
+                ans.append(cur.copy())
+                return
+
+            cur.append(nums[i])
+            dfs(i+1)
+
+            cur.pop()
+            dfs(i+1)
+
+        dfs(0)
         return ans
+        
