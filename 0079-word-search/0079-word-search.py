1ad4cf8604eb51d0946442518d4248d2eacb1ec5
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-02-10 21:15:16 -0500

    Time: 3514 ms (75.46%), Space: 17.8 MB (60.44%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,34 +1,31 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        def dfs(i, j, k):
-            
 
-            if not (0 <= i < len(board)) or \
-               not (0 <= j < len(board[0])) or \
-               board[i][j] != word[k]:  # Invalid path
-                # print('wrong', i, j, k)
-                return False  # Return False here
+        def dfs(i,j,cnt):
+            if (i < 0 or 
+            i >= len(board) or
+            j < 0 or 
+            j >= len(board[0]) or
+            cnt >= len(word) or
+            board[i][j] != word[cnt]):
+                return False
 
-            # print('true', i, j, k)
+            if cnt == len(word)-1:
+                return True
 
             tmp = board[i][j]
             board[i][j] = '#'
-            # Explore all 4 directions
-            if k == len(word) - 1: 
-                # print('oh yeah')
-                return True  # Found the word
-
-            if dfs(i + 1, j, k + 1) or \
-               dfs(i - 1, j, k + 1) or \
-               dfs(i, j + 1, k + 1) or \
-               dfs(i, j - 1, k + 1):
-                return True  # Return True if any direction works
+            
+            if dfs(i+1,j,cnt+1) or dfs(i-1,j,cnt+1) or dfs(i,j-1,cnt+1) or dfs(i,j+1,cnt+1):
+                return True
             board[i][j] = tmp
-            return False  # Return False if none of the directions work
 
+
+        
         for i in range(len(board)):
             for j in range(len(board[0])):
-                if dfs(i, j, 0): 
+                if dfs(i,j,0):
                     return True
-        
+
         return False
+        
