d08c09f28f88b3fee607c37e8d0b80836a2b4c7b
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-02-17 10:19:52 -0500

    Time: 0 ms (100%), Space: 18.1 MB (71.86%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -4,19 +4,19 @@ class Solution:
         for i in range(len(matrix)):
             if matrix[i][0] <= target <= matrix[i][-1]:
                 row = i
-                
+                break 
+        mid = 0
         l, r = 0, len(matrix[row]) - 1
         
-        while(l<=r):
-            mid = (l + r) // 2
-            print(l,r)
-            print('row:',row)
-            print('mid:',mid)
-            if matrix[row][mid] == target: return True
+        while (l<=r):
+            mid = (l+r) // 2
+            if matrix[row][mid] == target:
+                return True
 
             elif matrix[row][mid] < target:
                 l = mid + 1
 
-            elif matrix[row][mid] > target:
+            elif matrix[row][mid] > target: 
                 r = mid - 1
+
         return False
