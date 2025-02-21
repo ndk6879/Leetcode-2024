c0bad2aba29782a61f70b0666009ac925039b9e2
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-02-21 00:16:02 -0500

    Time: 27 ms (36.3%), Space: 18 MB (15.13%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -5,24 +5,24 @@ class Solution:
         ans = []
         cur = []
 
-        def dfs(i):
-            if sum(cur) == target:
+        def dfs(i, total):
+            if total == target:
                 if cur not in ans:
                     ans.append(cur.copy())
                     return
                            
 
-            if i >= len(candidates) or sum(cur) > target: 
+            if i >= len(candidates) or total > target: 
                 return False
 
             cur.append(candidates[i])
             
-            dfs(i+1)
+            dfs(i+1, candidates[i] + total)
             cur.pop()
             while i + 1 < len(candidates) and candidates[i] == candidates[i+1]:
                 i += 1
-            dfs(i+1)
+            dfs(i+1, total)
             
 
-        dfs(0)
+        dfs(0, 0)
         return ans
