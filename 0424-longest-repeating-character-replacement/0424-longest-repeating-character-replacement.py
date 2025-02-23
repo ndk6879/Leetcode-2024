098a5224beb9ff18d593d3f8c66df0806a34e801
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-02-23 01:06:35 -0500

    Time: 178 ms (31.72%), Space: 18.1 MB (48.48%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,16 +1,20 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        
-        hashMap = {}
+
         l = 0
-        cur = ''
+        hashMap = {}
         ans = 0
+
         for r in range(len(s)):
+            hashMap[s[r]] = 1 + hashMap.get(s[r],0)
 
-            hashMap[s[r]] = hashMap.get(s[r],0) + 1
-
-            while (r + 1 - l) - max(hashMap.values()) > k:
+            while ((r + 1 - l) - max(hashMap.values())) > k:
                 hashMap[s[l]] -= 1
+                if hashMap[s[l]] == 0:
+                    del hashMap[s[l]]
                 l += 1
+
+
             ans = max(ans, r + 1 - l)
+
         return ans
