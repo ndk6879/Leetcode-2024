c4718ab7b0f04a18636d514687fdd618f4a1abe4
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-02-25 22:00:44 -0500

    Time: 105 ms (90.6%), Space: 78 MB (96.29%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -8,21 +8,17 @@ class LRUCache:
         
 
     def get(self, key: int) -> int:
-
-        if key in self.cache:
-            self.cache.move_to_end(key)
-            return self.cache[key]
-
-        return -1 
-        
+        if not key in self.cache:
+            return -1
+        self.cache.move_to_end(key)
+        return self.cache[key]
 
     def put(self, key: int, value: int) -> None:
         self.cache[key] = value
         self.cache.move_to_end(key)
+
         if len(self.cache) > self.capacity:
             self.cache.popitem(last = False)
-
-
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
