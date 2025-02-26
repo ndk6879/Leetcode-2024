45e62c580ac9df3935ae76294b65866191dee8a4
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-02-25 19:47:51 -0500

    Time: 1 ms (7.91%), Space: 17.8 MB (41.83%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,25 +1,31 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if not digits:  # 빈 입력 예외 처리
-            return []
-
+        if digits == "": return []
         phone = {
             "2": "abc", "3": "def", "4": "ghi", "5": "jkl",
             "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"
         }
 
-        res = []
-        cur = []  # 전역 변수로 사용
-
-        def dfs(index):
-            if index == len(digits):  # 조합 완성되면 추가
-                res.append(cur.copy())  # 복사 후 추가
+        ans = []
+        cur = []
+        def dfs(i):
+            if len(cur) == len(digits):
+                tmp = ''.join(cur.copy())
+                ans.append(tmp)
                 return
+            
+            if i >= len(digits): return
 
-            for char in phone[digits[index]]:  # 해당 숫자의 문자들 탐색
-                cur.append(char)  # 문자 추가
-                dfs(index + 1)  # 다음 숫자로 이동
-                cur.pop()  # 백트래킹 (이전 상태로 되돌리기)
+            for c in phone[digits[i]]:
+                cur.append(c)
+                dfs(i+1)
+                cur.pop()
+                dfs(i+1)
 
         dfs(0)
-        return ["".join(c) for c in res]
+        return ans
+
+        
+
+
+        
