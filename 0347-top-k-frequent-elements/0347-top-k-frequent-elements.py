d305160197891cb79d176d6675cb5cefc7db35bf
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-02-25 23:30:52 -0500

    Time: 53 ms (5.04%), Space: 24.3 MB (13.07%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,25 +1,29 @@
 
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        adict = {}
-        for num in nums:
-            if num not in adict:
-                adict[num] = 0
-            else:
-                adict[num] += 1
         
+        hashMap = {}
+        for num in nums:
+            hashMap[num] = hashMap.get(num,0) + 1
+
+        print('hashMap:',hashMap)
+
         arr = [[] for _ in range(len(nums))]
 
-        for num in adict:
-            occurrence = adict[num]
-            arr[occurrence].append(num)
+        for key,v in hashMap.items():
+            arr[v-1].append(key)
 
+        print('arr:',arr)
 
         ans = []
-        for i in range(len(arr)-1,-1,-1):
-            if len(arr[i]) > 0:
-                while arr[i]:
-                    cur = arr[i].pop()
-                    ans.append(cur)
-                    k -= 1
-                    if k == 0: return ans
+        for i in range(len(arr) -1 , -1 , -1):
+            cur = arr[i]
+            print('cur:',cur)
+            while cur:
+                tmp = cur.pop()
+                ans.append(tmp)
+                print('ans:',ans,k)
+                k-= 1
+            if k == 0:
+                return ans
