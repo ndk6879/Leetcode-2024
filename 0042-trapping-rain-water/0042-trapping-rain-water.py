8985460328e43e1f93f6b3d3f0f295ce14963813
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-02-26 14:56:23 -0500

    Time: 11 ms (74.8%), Space: 19.4 MB (40%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -5,6 +5,8 @@ class Solution:
         l, r = 0, len(height) - 1
         maxL, maxR = height[l], height[r]
         ans = 0
+
+        #height의 각 위치에서 값은 min(maxL,maxR) - height[i]. min(maxL, maxR)로 구해야 index 4,5에서 알맞은 값을 구할수있음.
         while(l<r):
             if maxL < maxR:
                 l += 1
