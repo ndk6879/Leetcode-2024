08e8363d0336b46631845968cfd31b676f0a54fc
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-02-28 01:00:55 -0500

    Time: 0 ms (100%), Space: 17.6 MB (84.67%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -5,7 +5,10 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        
+
+#        아래 두줄로 나눠 선언하면 ans,dummy가 다른 ListNode를 가르키는거임 그래서 한줄로 합쳐야함
+        # ans = ListNode(0, head)
+        # dummy = ListNode(0, head)     
         ans = dummy = ListNode(0,head)        
 
         #1. head를 n만큼 앞에서 옮긴다.
