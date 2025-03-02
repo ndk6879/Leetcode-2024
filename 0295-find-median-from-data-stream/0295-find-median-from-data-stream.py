d4242e42a4a19421ecf67adde559312f3c25371a
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-03-01 21:21:44 -0500

    Time: 159 ms (38.08%), Space: 39.5 MB (63.98%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -1,20 +1,27 @@
-import heapq
-
 class MedianFinder:
 
     def __init__(self):
-        self.maxHeap = []  # 최대 힙 (음수값 저장) → 왼쪽 절반
-        self.minHeap = []  # 최소 힙 → 오른쪽 절반
+        self.maxHeap = [] #first - half
+        self.minHeap = [] # second - half
+        
 
     def addNum(self, num: int) -> None:
-        heapq.heappush(self.maxHeap, -num)  # -음수값 저장 → Python은 min-heap만 지원
-        heapq.heappush(self.minHeap, -heapq.heappop(self.maxHeap))  # 큰 값은 minHeap으로 이동
+        heapq.heappush(self.maxHeap, -num)
+        heapq.heappush(self.minHeap, -heapq.heappop(self.maxHeap))
 
-        if len(self.maxHeap) < len(self.minHeap):  # 균형 유지 (maxHeap이 항상 크거나 같아야 함)
-            heapq.heappush(self.maxHeap, -heapq.heappop(self.minHeap))
+        if len(self.maxHeap) < len(self.minHeap):
+            heapq.heappush(self.maxHeap, -heapq.heappop(self.minHeap)  )
+
 
     def findMedian(self) -> float:
-        if len(self.maxHeap) > len(self.minHeap):  # 홀수 개일 때 → maxHeap 루트값이 중앙값
+        if len(self.maxHeap) > len(self.minHeap):
             return -self.maxHeap[0]
-        else:  # 짝수 개일 때 → maxHeap과 minHeap 루트값 평균
+        
+        else:
             return (-self.maxHeap[0] + self.minHeap[0]) / 2
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
