4c71a580ad81aa2aebb7df34f30d56f45631d964
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-03-04 03:53:40 -0500

    Time: 4 ms (51.37%), Space: 19.5 MB (32.42%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -10,24 +10,21 @@ class Solution:
             crsMap[crs].append(preq)
 
 
-        visit = [0] * numCourses
+        visit = [0] * (numCourses)
         ans = []
         def dfs(i):
+            if visit[i] == 1: return False
             if visit[i] == 2: return True
-            if visit[i] == 1:
-                return False
 
             visit[i] = 1
             for pre in crsMap[i]:
-                dfs(preq)
+                if not dfs(pre): return False
 
             visit[i] = 2
             ans.append(i)
-            crsMap[i] = []
             return True
 
-        for i in range(numCourses):
-            dfs(i)
-
+        for n in range(numCourses):
+            if not dfs(n): return []
 
         return ans
