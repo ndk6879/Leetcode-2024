399ad7c12e5f4abe5d3afeadcc1584f841dad527
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-03-04 05:11:11 -0500

    Time: 80 ms (98.86%), Space: 58.2 MB (11.67%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -1,32 +1,29 @@
 class RandomizedSet:
 
     def __init__(self):
-        self.hashMap = {} # to store val
-        self.arr = [] # to sotre val and ind
+        self.hashMap = {}
+        self.arr = []
         
 
     def insert(self, val: int) -> bool:
-        if val in self.hashMap:
-            return False
-        
-        self.hashMap[val] = len(self.arr)
-        self.arr.append(val)
-        return True
-        
+        if val not in self.hashMap: 
+            self.hashMap[val] = len(self.arr)
+            self.arr.append(val)
+            return True
+        return False
 
     def remove(self, val: int) -> bool:
         if val not in self.hashMap: return False
-
-        
         ind = self.hashMap[val]
-        lastElement = self.arr[-1]
+        last = self.arr[-1]
 
-        self.arr[ind] = lastElement
-        self.hashMap[lastElement] = ind
-
+        self.arr[ind] = last
+        self.hashMap[last] = ind
+        
         self.arr.pop()
         del self.hashMap[val]
         return True
+        
 
     def getRandom(self) -> int:
         return random.choice(self.arr)
