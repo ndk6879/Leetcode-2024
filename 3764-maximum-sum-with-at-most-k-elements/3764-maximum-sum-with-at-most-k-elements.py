7e3d1b2da7296e7dfcd32ac3f1febdf7987a1c7d
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-03-05 05:22:40 -0500

    Time: 535 ms (20.84%), Space: 50.6 MB (23.5%) - LeetHub

--- 3764-maximum-sum-with-at-most-k-elements/3764-maximum-sum-with-at-most-k-elements.py
+++ 3764-maximum-sum-with-at-most-k-elements/3764-maximum-sum-with-at-most-k-elements.py
@@ -3,11 +3,8 @@ import heapq
 class Solution:
     def maxSum(self, grid: List[List[int]], limits: List[int], k: int) -> int:
         arr = []
-
         for i in range(len(grid)):
-            arr += heapq.nlargest(limits[i] , grid[i])
-            
-        heapq.heapify(arr)
-
+            cur = heapq.nlargest(limits[i], grid[i])
+            arr += cur
 
         return sum(heapq.nlargest(k,arr))
