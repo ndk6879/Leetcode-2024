367e1f379a782c197b40e5e5b15ca38ee753e54a
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-03-11 01:33:41 -0400

    Time: 11 ms (31.31%), Space: 21 MB (42.97%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,19 +1,26 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort()
+        print('intervals:',intervals)
         cur = intervals[0]
         ans = []
 
-        for i in range(len(intervals)):
 
-            if cur[0] > intervals[i][1]:
-                ans.append(intervals[i])
+        for i in range(1,len(intervals)):
 
-            elif cur[1] < intervals[i][0]:
+            #1. NOT overlap
+            if cur[1] < intervals[i][0]:
                 ans.append(cur)
                 cur = intervals[i]
 
+            #2. not 
+            elif cur[0] > intervals[i][1]:
+                ans.append(cur[i])
+
+
+            #3. overlap
+            
             else:
-                cur = (min(cur[0],intervals[i][0]), max(cur[1], intervals[i][1]))
+                cur = [min(cur[0], intervals[i][0]), max(cur[1], intervals[i][1])]
         ans.append(cur)
         return ans
