0a5470349509da08700095de8d1810160287bc87
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-03-12 05:02:57 -0400

    Time: 0 ms (100%), Space: 18.1 MB (59.97%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,33 +1,31 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        A, B = nums1, nums2
 
+        A,B = nums1, nums2
         if len(A) > len(B):
-            A,B = B, A
+            A,B = B,A
 
+        
         start, end = 0, len(A) - 1
 
         while True:
             mid1 = (start + end) // 2
-            mid2 = (len(A) + len(B))//2 - mid1 - 2
+            mid2 = (len(A) + len(B)) // 2 - mid1 - 2
 
-            leftA = A[mid1] if mid1 >=0 else float("-infinity")
-            rightA = A[mid1+1] if mid1+1 < len(A) else float("infinity")
-            leftB = B[mid2] if mid2 >=0 else float("-infinity")
+            leftA = A[mid1] if mid1 >= 0 else float("-infinity")
+            rightA = A[mid1+1] if mid1+1 < len(A) else float("infinity") 
+            leftB = B[mid2] if mid2 >= 0 else float("-infinity")
             rightB = B[mid2+1] if mid2+1 < len(B) else float("infinity")
 
-            # print('mid1:',mid1)
             if leftA <= rightB and leftB <= rightA:
-                
                 if (len(A) + len(B)) % 2 == 0:
                     return (max(leftA,leftB) + min(rightA,rightB)) / 2
 
                 else:
                     return min(rightA,rightB)
 
-
             elif leftA > rightB:
-                end = mid1 - 1
+                end = mid1 -1
+            
             else:
                 start = mid1 + 1
-
