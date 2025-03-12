1f055360cb4f2bf22d99fd7d14c9c3a968658c0d
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-03-11 23:44:24 -0400

    Time: 20 ms (27.36%), Space: 22.6 MB (32.32%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,18 +1,17 @@
-from collections import Counter
-
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
         ans = {}
-        
         for string in strs:
             tmp = [0] * 26
-            for s in string:
-                tmp[ord(s) - ord('a')] += 1
+            
+            for i in string:
+                tmp[ord(i) - ord('a')] += 1
 
             if tuple(tmp) not in ans:
                 ans[tuple(tmp)] = [string]
 
             else:
                 ans[tuple(tmp)].append(string)
-        
+
         return list(ans.values())
