bdc573c2020ccc6c983cc5877620f5586f225dee
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-03-12 02:08:42 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.py
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -1,22 +1,19 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        l, r = 1, max(piles)
+        l,r = 0, max(piles)
         ans = 0
 
-        while (l <= r):
+        while(l<=r):
+            
             k = (l+r) // 2
-
-
             tmp = 0
             for pile in piles:
                 tmp += ceil(pile / k)
 
-
             if tmp <= h:
                 ans = k
-                r = k - 1
-            
+                r = k -1
+
             else:
                 l = k + 1
-
         return ans
