9d992a929db8ac3017f7c940c6a3b0b7b7487ac5
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-03-17 06:08:05 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1023-time-based-key-value-store/1023-time-based-key-value-store.py
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.py
@@ -2,28 +2,29 @@ class TimeMap:
 
     def __init__(self):
         self.hash = {}
+        
 
     def set(self, key: str, value: str, timestamp: int) -> None:
         if key not in self.hash:
             self.hash[key] = [[timestamp, value]]
         else:
-            self.hash[key].append([timestamp, value])
+            self.hash[key].append([timestamp,value])
         
 
     def get(self, key: str, timestamp: int) -> str:
+        ans = 0
+        if key not in self.hash: return ''
 
-        if key not in self.hash:
-            return ''
-        
-        l, r = 0, len(self.hash[key]) - 1
-        ans = ''
-        while(l <= r):
+        l, r = 0, len(self.hash[key])-1
+        ans = 0
+
+        while (l <= r):
             mid = (l + r) // 2
-            values = self.hash[key][mid]
-            if values[0] <= timestamp:
-                ans = values[1]
+            # print('self.hash[key][mid][0]:',self.hash[key])
+            if self.hash[key][mid][0] <= timestamp:
+                ans = self.hash[key][mid][1]
                 l = mid + 1
-
+            
             else:
                 r = mid - 1
         return ans
