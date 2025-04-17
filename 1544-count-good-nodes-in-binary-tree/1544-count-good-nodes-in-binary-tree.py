3fe24bd638b123f1cea57fa73244f3256001a5ee
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-04-17 15:36:28 -0400

    Time: 140 ms (65.68%), Space: 32.2 MB (63.19%) - LeetHub

--- 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
@@ -4,31 +4,22 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-'''
-이것도 DFS를 이용해 풀면됨.
-base case와 recursive case때 어떻게 할지 생각하면서 풀면 쉽게 가능.
-햇갈리면 이전에 543. Diameter of Binary Tree, 110. Balanced Binary Tree 같은 DFS문제 ㄱㄱ
-'''
-
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
         self.ans = 0
+        def dfs(node, val):
+            #base case
+            if not node: return
 
-
-        def dfs(root, rootVal):
-            if not root: return 0
-
-            if root.val >= rootVal:
+            if node.val >= val:
                 self.ans += 1
 
-            if root.left:
-                dfs(root.left, max(rootVal, root.left.val))
-            if root.right:
-                dfs(root.right, max(rootVal, root.right.val))
-            
+            #recursive case
+            greatestValue = max(node.val, val)
+            left = dfs(node.left, greatestValue) 
+            right = dfs(node.right, greatestValue)
+            return
 
-        dfs(root,root.val)
-
+        dfs(root, root.val)
         return self.ans
         
