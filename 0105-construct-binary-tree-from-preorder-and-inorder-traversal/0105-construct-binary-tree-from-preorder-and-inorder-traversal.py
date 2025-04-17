f1f45c1712a7f28a05aaa2b09ae52aef2a1446fc
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-04-17 16:39:10 -0400

    Time: 91 ms (48.23%), Space: 89.3 MB (20.21%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,10 +6,15 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+    
+        #preorder = root - [left.subtree] - [right.subtree]
+        #inorder = [left.subtree] - root - [right.subtree]
+
+        
         if not preorder and not inorder: return
 
         root = TreeNode(preorder[0])
-        rootIndex = inorder.index(preorder[0])
+        rootIndex = inorder.index(preorder[0]) #left subtree에 몇개의 노드가 있는지 알수있음
         root.left = self.buildTree(preorder[1:1+rootIndex], inorder[:rootIndex])
         root.right = self.buildTree(preorder[1+rootIndex:], inorder[1+rootIndex:])
         return root
