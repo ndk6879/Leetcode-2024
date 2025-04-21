d1f7ba0ac56549a878ff82a4361958a291f52d23
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-04-21 14:58:31 -0400

    Time: 6806 ms (5.23%), Space: 36.6 MB (5.03%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -8,35 +8,32 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        
+        #1. move a pointer to the second half 
         slow, fast = head, head.next
-
-        #1. find the second half
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-
 
         second = slow.next
         slow.next = None
         prev = None
 
+        
+        
 
+        
+        #2. reverse the second half
         while second:
             tmp = second.next
             second.next = prev
             prev = second
             second = tmp
+        print(prev) 
         
         first, second = head, prev
+        #3. reorder
         while first and second:
             tmp1, tmp2 = first.next, second.next
-
             first.next = second
             second.next = tmp1
             first, second = tmp1, tmp2
-
-
-        #2. reverse the second half
-
-        #3. reorder
