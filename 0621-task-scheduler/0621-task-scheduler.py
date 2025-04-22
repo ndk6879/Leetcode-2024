1bc5b2b25c713a09fb59ae9f01111db71673fe98
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-04-22 13:04:46 -0400

    Time: 138 ms (47.01%), Space: 19.3 MB (26.63%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,22 +1,27 @@
-from collections import Counter
+from collections import Counter, deque
 
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        # Count the occurrences of each task
-        task_counts = Counter(tasks)
-      
-        # Find the maximum frequency of any task
-        max_freq = max(task_counts.values())
-      
-        # Count how many tasks have the maximum frequency
-        max_freq_tasks_count = sum(freq == max_freq for freq in task_counts.values())
-      
-        # Calculate the number of idle states needed, which is defined by the formula:
-        # (maximum frequency of any task - 1) * (n + 1) which gives the spaces between repetitions of the same task
-        idle_time = (max_freq - 1) * (n + 1)
-      
-        # Add the count of most frequent tasks to idle time to get minimum length of the task schedule
-        min_length = idle_time + max_freq_tasks_count
-      
-        # Return the maximum of the length of tasks (if no idle time is necessary) and calculated minimum length
-        return max(len(tasks), min_length)
+        
+        counter = Counter(tasks)
+
+        maxHeap = [-cnt for cnt in counter.values()]
+        queue = deque([]) # [cnt, idletime]
+        heapq.heapify(maxHeap)
+
+        time = 0
+        while maxHeap or queue:
+            time += 1
+
+            if maxHeap:
+                cur = heapq.heappop(maxHeap) + 1
+                if cur != 0:
+                    queue.append([cur, time+n])
+
+            if queue and queue[0][1] == time:
+                heapq.heappush(maxHeap,queue.popleft()[0])
+
+        
+
+        
+        return time
