56063345b66d6700883500962bd0ba80bb524281
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-04-25 17:20:48 -0400

    Time: 50 ms (5.14%), Space: 18 MB (14.32%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,28 +1,23 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        
         candidates.sort()
-
         ans = []
-        cur = []
 
-        def dfs(i, total):
-            if total == target:
-                if cur.copy() not in ans:
-                    ans.append(cur.copy())
-                    return
+        def dfs(index, path):
+            if sum(path) == target:
+                ans.append(path[:])
+                return
 
-            elif i >= len(candidates) or total > target:
-                return False
+            if sum(path) > target or index >= len(candidates):
+                return
 
-            cur.append(candidates[i])
-            
-            dfs(i+1, total + candidates[i])
-            cur.pop()
-            while i + 1 < len(candidates) and candidates[i] == candidates[i+1]:
-                i += 1
-            dfs(i+1, total)
+            path.append(candidates[index])
+            dfs(index+1, path)
+            path.pop()
+            while index+1 < len(candidates) and candidates[index]==candidates[index+1]:
+                index += 1
+            dfs(index+1,path)
 
-
-        dfs(0, 0)
-
+        dfs(0,[])
         return ans
