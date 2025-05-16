74813c2c34b656a8e2d4554b97c684249aa802bf
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-05-16 02:39:20 -0400

    Time: 38 ms (5.16%), Space: 18 MB (7.49%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.py
+++ 0778-reorganize-string/0778-reorganize-string.py
@@ -1,25 +1,26 @@
-from collections import Counter
 import heapq
+from collections import Counter
 
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        counter = Counter(s)
-        arr = [[-v,k] for k,v in counter.items()]
-        heapq.heapify(arr)
-
-        print('arr:',arr)
-
-        ans = ''
-        tmp = [0,'']
-        while arr:
-            cnt, character = heapq.heappop(arr)
-            if tmp[0] < 0:
-                heapq.heappush(arr,tmp)
-            
-            tmp = [cnt + 1,character]
-            ans += character
-            
-            print('ans:',ans,'\narr:',arr,'\n')
-
         
+        ans = ''
+        counter = Counter(s)
+        heap = []
+
+        for string in counter:
+            print(string,counter[string])
+            heapq.heappush(heap,[counter[string] * -1 , string ])
+
+        tmp = [0, '']
+        while heap:
+            cnt, string = heapq.heappop(heap)
+            print(cnt,string)
+            if tmp[0] < 0:
+                heapq.heappush(heap,tmp)
+
+            tmp = [cnt + 1, string]
+
+
+            ans += string
         return ans if len(ans) == len(s) else ""
