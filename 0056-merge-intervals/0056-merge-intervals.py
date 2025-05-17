d94818afe0c19ef244f9c3d44e62270244928773
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-05-16 22:32:20 -0400

    Time: 11 ms (31.13%), Space: 21 MB (43.77%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,18 +1,21 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        # S. 이끌기 0. 어떻게 풀지 method 생각  1. edge case 2. psuedo code 3. TC#
         intervals.sort()
-        cur = intervals[0]
         ans = []
+        cur = intervals[0]
 
         for i in range(1,len(intervals)):
-            if cur[0] > intervals[i][1]:
-                ans.append(intervals[i])
 
-            elif cur[1] < intervals[i][0]:
+            #1. if overlap
+            if cur[1] >= intervals[i][0]:
+                cur = [ min(cur[0], intervals[i][0])   , max(cur[1],intervals[i][1]) ]
+
+            #2. not overlap -> compare interval and cur 
+            elif cur[1] < intervals[i] [0]:
                 ans.append(cur)
                 cur = intervals[i]
 
-            else:
-                cur = [min(cur[0],intervals[i][0]), max(cur[1], intervals[i][1])]
+
         ans.append(cur)
         return ans
