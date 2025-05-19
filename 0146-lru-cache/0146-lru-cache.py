7e73d5a0d08d62ff3ca1a175fa6df339c87d2a6c
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-05-19 09:39:05 -0400

    Time: 120 ms (77.8%), Space: 78.1 MB (95.73%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -3,25 +3,23 @@ from collections import OrderedDict
 class LRUCache:
 
     def __init__(self, capacity: int):
+        self.capacity = capacity
         self.cache = OrderedDict()
-        self.size = capacity
         
 
     def get(self, key: int) -> int:
         if key in self.cache:
             self.cache.move_to_end(key)
             return self.cache[key]
-        else:
-            return -1
+        return -1
 
     def put(self, key: int, value: int) -> None:
-        
+
         self.cache[key] = value
         self.cache.move_to_end(key)
-
-        if len(self.cache) > self.size:
+        
+        if len(self.cache) > self.capacity:
             self.cache.popitem(last=False)
-
         
 
 
