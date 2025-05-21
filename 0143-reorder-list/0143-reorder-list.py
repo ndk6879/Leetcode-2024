53d45f76dd2d0894f0421e83f8cd45f1b7d8a777
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-05-21 19:30:20 -0400

    Time: 6701 ms (5.02%), Space: 36.6 MB (5.22%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -8,32 +8,43 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        #1. move a pointer to the second half 
+        
+        #1. find mid pointer
         slow, fast = head, head.next
+
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
 
         second = slow.next
-        slow.next = None
+
+        #맨마지막에 오는 3의 next를 none으로 만듬. example 2에
+        slow.next = None #example1에선 2가 slow고 .next를해서 first half를 3과 연결끊음.
         prev = None
 
-        
-        
 
-        
         #2. reverse the second half
         while second:
             tmp = second.next
             second.next = prev
             prev = second
             second = tmp
-        print(prev) 
-        
+
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+        print(prev)
+
+
+        #3. link
+
         first, second = head, prev
-        #3. reorder
         while first and second:
             tmp1, tmp2 = first.next, second.next
             first.next = second
             second.next = tmp1
             first, second = tmp1, tmp2
+        
+        
