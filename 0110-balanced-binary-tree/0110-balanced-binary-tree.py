5a35154c83b6163dd24528fc26219309d183e390
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-05-21 22:25:26 -0400

    Time: 3 ms (67.91%), Space: 19 MB (27.81%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -4,29 +4,19 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-'''
-Diameter of Binary Tree를 DFS로 풀줄알면 개쉬움.
-DFS의 1. base case와 2. recursive case를 고려
-그리고 balanced인지 확인
-'''
-
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         
         self.balanced = True
-
         def dfs(node):
             if not node: return 0
-
             left = dfs(node.left)
             right = dfs(node.right)
 
-            if abs(left - right) > 1: 
+            if abs(left - right) > 1:
                 self.balanced = False
 
             return 1 + max(left,right)
 
         dfs(root)
-
         return self.balanced
