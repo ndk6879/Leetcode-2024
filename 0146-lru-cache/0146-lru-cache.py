71605d34901ae905b337bc0951ce90e71f8512aa
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-05-22 16:15:36 -0400

    Time: 105 ms (89.42%), Space: 78.1 MB (95.82%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,59 +1,26 @@
-class Node:
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-        self.next = None
-        self.prev = None
-
+from collections import OrderedDict
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cache = {} # {key : Node()}
+        self.cache = OrderedDict()
         self.capacity = capacity
-
-        self.left = Node(0,0)
-        self.right = Node(0,0)
-        self.left.next = self.right
-        self.right.prev = self.left
-        
-    def remove(self, node):
-        left = node.prev
-        right = node.next
-        left.next = right
-        right.prev = left
-        
-
-    def insert(self, node):
-        prev, nxt = self.right.prev, self.right
-        
-        prev.next = node
-        node.prev = prev
-        nxt.prev = node
-        node.next = nxt
 
     def get(self, key: int) -> int:
         if key in self.cache:
-            node = self.cache[key]
-            self.remove(node)
-            self.insert(node)
-            return node.value
-        return -1
-        
+            self.cache.move_to_end(key)
+            return self.cache[key]
+
+        return -1    
 
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self.remove(self.cache[key])
         
-        node = Node(key, value)
-        self.cache[key] = node
-        self.insert(node)
+        self.cache[key] = value
+        self.cache.move_to_end(key)
+
         if len(self.cache) > self.capacity:
-            # remove from the list and delete from the map
-            lru = self.left.next
-            self.remove(lru)
-            del self.cache[lru.key]
-        
+            self.cache.popitem(last = False)
+                    
 
 
 # Your LRUCache object will be instantiated and called as such:
