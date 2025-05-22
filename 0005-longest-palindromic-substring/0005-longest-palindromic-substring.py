b90a9ac71420ac5fb6af1d9037f2ee88cb5fd36e
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-05-22 02:34:59 -0400

    Time: 372 ms (45.02%), Space: 17.8 MB (83.99%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,28 +1,30 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        res = 0
         
+        
+        ans = 0
+        ansLen = ''
         for i in range(len(s)):
 
-            l,r = i,i
+            #odd
+            l, r = i,i
 
-            while(l >= 0 and r < len(s) and s[l]==s[r]):
-                if (res < r + 1 - l):
-                    
-                    string = s[l:r+1]
-                    res = max(res, r + 1 - l)
+            while (l >= 0 and r < len(s) and s[l] == s[r]):
+                if len(ansLen) < (r + 1 - l):
+                    ans = max(ans, r + 1 - l)
+                    ansLen = s[l:r+1]
+
                 l -= 1
-                r += 1
-
-            l,r = i,i+1
-
-            while(l >= 0 and r < len(s) and s[l]==s[r]):
-                if (res < r + 1 - l):
-                    
-                    string = s[l:r+1]
-                    res = max(res, r + 1 - l)
-                l -= 1
-                r += 1
-
+                r +=1
             
-        return string
+
+            #even
+            l, r = i, i+1
+            while (l >= 0 and r < len(s) and s[l] == s[r]):
+                if len(ansLen) < (r + 1 - l):
+                    ans = max(ans, r + 1 - l)
+                    ansLen = s[l:r+1]
+
+                l -= 1
+                r +=1
+        return ansLen
