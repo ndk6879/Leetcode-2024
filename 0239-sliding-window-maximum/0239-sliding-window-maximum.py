2fda47f90cd9b070e7ace321eda5677f47b38978
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-05-22 15:05:22 -0400

    Time: 195 ms (32.6%), Space: 31.3 MB (63.74%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -2,22 +2,27 @@ from collections import deque
 
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        res = []
-        q = deque()  # store indices
+        
+        q = deque([])
+        l, r = 0,0
+        ans = []
 
-        for i in range(len(nums)):
-            # 1. 윈도우 벗어난 인덱스 제거
-            if q and q[0] <= i - k:
-                q.popleft()
+        while (r < len(nums)):
 
-            # 2. 현재 값보다 작거나 같은 인덱스는 뒤에서 제거
-            while q and nums[q[-1]] < nums[i]:
+            #pop smaller value
+            while (q and nums[q[-1]] < nums[r]):
                 q.pop()
 
-            q.append(i)
+            q.append(r)
 
-            # 3. 첫 윈도우가 만들어진 시점부터 최대값 기록
-            if i >= k - 1:
-                res.append(nums[q[0]])
+            #remove l out of window
+            if (q and q[0] < l):
+                q.popleft()
 
-        return res
+            #add in window
+            if (r+1) >= k:
+                ans.append(nums[q[0]])
+                l += 1
+            r += 1
+
+        return ans
