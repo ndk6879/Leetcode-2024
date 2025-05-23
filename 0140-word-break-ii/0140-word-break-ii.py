b32c6acceb83ae34ae0063a0c7584fb7d5c43389
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-05-23 11:04:09 -0400

    Time: 0 ms (100%), Space: 17.8 MB (42.27%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.py
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -1,22 +1,24 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
         
+        ans = []
+        temp = []
 
-        res = []
-
-        def dfs(i, path):
-            if i == len(s):
-                tmp = ' '.join(path)
-                res.append(tmp)
+        def dfs(i,path):
+            print('path:',path)
+            if i >= len(s):
+                ans.append(' '.join(path))
                 return
 
             for j in range(i,len(s)):
-                string = s[i:j+1]
-                if string in wordDict:
-                    path.append(string)
-                    dfs(j+1,path)
-                    path.pop()
+                cur = s[i:j+1]
+                for word in wordDict:
+                    if cur == word:
+                        print('cur:',cur)
+                        path.append(word)
+                        dfs(j+1,path)
+                        path.pop()
 
-        dfs(0, [])
+        dfs(0,temp)
 
-        return res
+        return ans
