8bba96b9eea8ead826df7da164a8579bc53e8a8f
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-05-23 03:54:34 -0400

    Time: 43 ms (11.03%), Space: 19.6 MB (11.18%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -5,7 +5,7 @@ class Solution:
         visit = set()
         def dfs(i,j):
             if (i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or (i,j) in visit or grid[i][j] == 0):
-                return False
+                return 0
             visit.add((i,j))
             return (1 + dfs(i+1,j) + dfs(i-1,j) + dfs(i,j+1) + dfs(i,j-1))
         
