a1d885df43b759a4ff4ba873a81945b1d0e3cf56
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-05-23 05:24:36 -0400

    Time: 5494 ms (19.72%), Space: 21.5 MB (74.45%) - LeetHub

--- 3751-maximum-frequency-after-subarray-operation/3751-maximum-frequency-after-subarray-operation.py
+++ 3751-maximum-frequency-after-subarray-operation/3751-maximum-frequency-after-subarray-operation.py
@@ -1,31 +1,24 @@
-from typing import List
-
 class Solution:
     def maxFrequency(self, nums: List[int], k: int) -> int:
-        # 1. 현재 배열에 이미 존재하는 K의 개수를 셈
-        starting_frequency = nums.count(k)
+        
+        
+        ans1 = nums.count(k)
+        ans2 = 0
 
-        # 2. 연속 구간에서 바꿔서 만들 수 있는 최대 K 개수
-        max_duplicates = 0
+        for i in range(1,51):
+            if i == k: continue
 
-        # 3. 가능한 모든 target 값을 시도 (1부터 50까지)
-        for target_value in range(1, max(nums)+1):
-            if target_value == k:
-                continue  # 원래 K는 이미 포함했으니까 패스
-
-            current_freq = 0  # 현재 구간에서 만들 수 있는 K 개수
-            current_max = 0   # 그 중 최대값 저장
-
+            frequency = 0
             for num in nums:
-                if num == target_value:
-                    current_freq += 1  # 이 숫자는 바꾸면 K가 됨
+
+                if num == i:
+                    frequency += 1
+
                 elif num == k:
-                    current_freq -= 1  # 이미 K인 숫자는 바꾸면 K가 사라짐
-
-                # 누적합이 0보다 작으면 버리고 새로 시작
-                current_freq = max(current_freq, 0)
+                    frequency -= 1
                 
-                max_duplicates = max(max_duplicates, current_freq)
-
-        # 최종 정답 = 기존 K + 새로 만든 K
-        return max_duplicates + starting_frequency
+                frequency = max(frequency,0)
+                
+                ans2 = max(ans2, frequency)
+        
+        return ans1 + ans2
