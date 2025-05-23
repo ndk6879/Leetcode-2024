5b6d653b916ea8d4f1d224840c37157bc5996061
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-05-23 02:13:11 -0400

    Time: 51 ms (52.7%), Space: 34.7 MB (18.76%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,31 +1,29 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
+        
         ans = []
 
-        def dfs(i, path):
+        def dfs(i,path):
             if len(s) == i:
                 ans.append(path[:])
                 return
-            
-            for j in range(i,len(s)): #i = start, j = end
+
+
+            for j in range(i,len(s)):
                 if self.palindrome(s,i,j):
-                    
                     path.append(s[i:j+1])
-                    print('path:',path)
                     dfs(j+1,path)
                     path.pop()
+                    
+        dfs(0,[])
 
-    
-        dfs(0, [])
         return ans
 
-    def palindrome(self,s,l,r):
+    def palindrome(self, s, l, r):
 
-        while l < r:
-            if s[l] != s[r]:
-                return False
+        while l <= r:
+            if s[l] != s[r]: return False
+
             l += 1
             r -= 1
-        
         return True
-
