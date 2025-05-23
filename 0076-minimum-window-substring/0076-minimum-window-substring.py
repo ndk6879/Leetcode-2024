28e84c43ffd7b61b8fdc653efe41ddce1af5c500
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-05-23 04:48:13 -0400

    Time: 142 ms (28.7%), Space: 18.3 MB (34.66%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -7,28 +7,32 @@ class Solution:
         
         need = Counter(t)
         have = {}
-        have_count = 0
-        res = ""
-        min_len = float('inf')
+        haveCount = 0
+
+        ans = ''
+        ansLen = float("inf")
+
         l = 0
-
         for r in range(len(s)):
-            char = s[r]
-            have[char] = have.get(char, 0) + 1
+            have[s[r]] = have.get(s[r],0) + 1
 
-            if char in need and have[char] == need[char]:
-                have_count += 1
+            c = s[r]
+            if c in need and have[c] == need[c]:
+                haveCount += 1
 
-            while have_count == len(need):
-                # 최소 길이 갱신
-                if r - l + 1 < min_len:
-                    min_len = r - l + 1
-                    res = s[l:r+1]
-                
-                # 왼쪽 포인터 줄이기
+            
+            #조건이 충족되면
+            while ( haveCount == len(need) ):
+                # 값 비교
+                if (r + 1 - l) < ansLen:
+                    ansLen = r + 1 -l
+                    ans = s[l:r+1]
+
+                #왼쪽 포인터 길이 조절
                 have[s[l]] -= 1
                 if s[l] in need and have[s[l]] < need[s[l]]:
-                    have_count -= 1
-                l += 1
+                    haveCount -= 1
 
-        return res
+                l+=1
+
+        return ans
