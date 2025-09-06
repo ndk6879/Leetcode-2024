1fa6814f0b9ff8850994e6f1db1de8e0787b70d4
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-09-06 19:10:08 -0400

    Time: 10 ms (74.9%), Space: 17.7 MB (86.66%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,24 +1,12 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        '''
-        #1. compare if backwards is same
-        #2. check if they have same dtionary to comapre same # of chr. 
-        '''
-        
-        dictS = {}
-        dictT = {}
+        sHashMap, tHashMap = {}, {}
 
-        for let in s:
-            if let not in dictS:
-                dictS[let] = 1
-            else:
-                dictS[let] += 1
-            
-        for let in t:
-            if let not in dictT:
-                dictT[let] = 1
-            else:
-                dictT[let] += 1
+        for i in s:
+            sHashMap[i] = sHashMap.get(i,0)+1
 
-        return dictT == dictS
-        
+
+        for i in t:
+            tHashMap[i] = tHashMap.get(i,0)+1
+
+        return sHashMap == tHashMap
