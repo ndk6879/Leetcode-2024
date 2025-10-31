e2785fde5512722e315bb1d773f326b18f4450b9
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-10-31 02:33:52 -0400

    Time: 3 ms (46.19%), Space: 23.3 MB (80.68%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -9,42 +9,33 @@ class Solution:
         Do not return anything, modify head in-place instead.
         """
         
-        #1. find mid pointer
-        slow, fast = head, head.next
 
+        #1. pointer on the second half
+        fast, slow = head.next, head
         while fast and fast.next:
-            slow = slow.next
             fast = fast.next.next
-
+            slow = slow.next
+        
+        print('slow:',slow.val)
         second = slow.next
+        slow.next = None
+        print('slow:',slow.val)
 
-        #맨마지막에 오는 3의 next를 none으로 만듬. example 2에
-        slow.next = None #example1에선 2가 slow고 .next를해서 first half를 3과 연결끊음.
-        prev = None
-
-
-        #2. reverse the second half
+        #2. reverse second half
+        tmp = None
         while second:
-            tmp = second.next
-            second.next = prev
-            prev = second
-            second = tmp
+            tmp2 = second.next
+            second.next = tmp
+            tmp = second
+            second = tmp2
 
-        while second:
-            tmp = second.next
-            second.next = prev
-            prev = second
-            second = tmp
-        print(prev)
+        #3. reorder
+        first, second = head, tmp
 
-
-        #3. link
-
-        first, second = head, prev
         while first and second:
-            tmp1, tmp2 = first.next, second.next
+            tmp1, tmp2 = first.next , second.next
             first.next = second
             second.next = tmp1
             first, second = tmp1, tmp2
-        
-        
+
+
