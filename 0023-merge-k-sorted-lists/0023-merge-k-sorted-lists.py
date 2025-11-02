6ff57159b7d5a617a76ebc32327c42d63f867a18
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-11-02 18:12:13 -0500

    Time: 7 ms (89.64%), Space: 20.1 MB (71.44%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -6,43 +6,51 @@
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
 
-        if len(lists) == 0: return None
-        
+        #1. merge two sub lists in lists arr
+        #2. update the lists with those two sub lists
+
+        if not lists:
+            return None
+
+
         while len(lists) > 1:
-            mergedLists = []
+            sub = []
             for i in range(0,len(lists),2):
+                
                 l1 = lists[i]
-                l2 = lists[i+1] if i + 1 < len(lists) else None
-                newList = self.mergeList(l1,l2)
-                mergedLists.append(newList)
-            lists = mergedLists
-            
+                l2 = lists[i+1] if (i + 1) < len(lists) else None
+
+                sub.append(self.mergeLists(l1,l2))
+            lists = sub
         return lists[0]
 
         
-    def mergeList(self,l1,l2):
+    def mergeLists(self,l1,l2):
 
         ans = cur = ListNode()
 
         while l1 and l2:
-            if l1.val >= l2.val:
-                cur.next = l2
-                cur = cur.next
-                l2 = l2.next
-
-            else:
+            if l1.val <= l2.val:
                 cur.next = l1
-                cur = cur.next
                 l1 = l1.next
 
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+        
         while l1:
             cur.next = l1
-            cur = cur.next
             l1 = l1.next
+            cur = cur.next
 
         while l2:
             cur.next = l2
-            cur = cur.next
             l2 = l2.next
-        
+            cur = cur.next
+
         return ans.next
+
+                
+        
+
