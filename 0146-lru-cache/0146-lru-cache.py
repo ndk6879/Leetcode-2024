75220d49bee7450e96225ae03e6feaa009ef1122
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-11-01 22:09:24 -0400

    Time: 68 ms (99.61%), Space: 78.1 MB (27.59%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,62 +1,26 @@
-class Node:
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-        self.prev = None
-        self.next = None
+from collections import OrderedDict
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cache = {} # key : Node
+        self.cache = OrderedDict()
         self.capacity = capacity
-
-        self.left = Node(0,0)
-        self.right = Node(0,0)
-
-        self.left.next = self.right
-        self.right.prev = self.left
-
-    def remove(self, node):
         
-        prev, nxt = node.prev, node.next
-
-        prev.next = nxt
-        nxt.prev = prev
-
-    def insert(self, node):
-        prev, nxt = self.right.prev, self.right
-
-        prev.next = node
-        node.prev = prev
-        node.next = nxt
-        nxt.prev = node
-
 
     def get(self, key: int) -> int:
         if key in self.cache:
-            self.remove(self.cache[key])
-            self.insert(self.cache[key])
-            return self.cache[key].value
-
+            self.cache.move_to_end(key)
+            return self.cache[key]
         return -1
-        
         
 
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self.remove(self.cache[key])
 
-        node = Node(key, value)
-        self.cache[key] = node
-        self.insert(node)
+        self.cache[key] = value
+        self.cache.move_to_end(key)
 
         if len(self.cache) > self.capacity:
-            
-            #lru = L
-            l = self.left.next
-            self.remove(l)
-            del self.cache[l.key]
+            self.cache.popitem(last = False)
         
 
 
