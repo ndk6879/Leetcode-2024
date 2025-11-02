097114c0975008e5b24c5bd41036fac7a7796df9
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-11-02 18:12:46 -0500

    Time: 12 ms (45.11%), Space: 20.1 MB (71.44%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -39,18 +39,9 @@ class Solution:
                 l2 = l2.next
             cur = cur.next
         
-        while l1:
-            cur.next = l1
-            l1 = l1.next
-            cur = cur.next
-
-        while l2:
-            cur.next = l2
-            l2 = l2.next
-            cur = cur.next
-
+       
+        cur.next = l1 or l2
         return ans.next
-
                 
         
 
