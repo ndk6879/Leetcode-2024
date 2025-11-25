52910607daea5c9ff1dbffe615afd36e301028b9
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-11-24 20:24:27 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -0,0 +1,22 @@
+from collections import Counter
+
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+
+        #1. create counter to count each # of elements 
+        #2. sort the hand
+        #3. use for loop to iterate over and check if there's next elment at each index
+
+
+        counter = Counter(hand)
+        hand.sort()
+        
+        for num in hand:
+            if counter[num]:
+                for i in range(num,num+groupSize):
+                    if i not in counter:
+                        return False
+
+                    counter[i] -= 1
+        
+        return True
