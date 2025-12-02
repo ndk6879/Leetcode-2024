1bd10ebe394d4b502a1dab99c82da51202493c68
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-12-01 20:41:44 -0500

    Time: 0 ms (100%), Space: 17.7 MB (82.21%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,17 +1,12 @@
-'''
-rob1, rob2가 dp 배열의 i-2, i-1 인덱스를 가리키고
-매 loop마다 오른쪽으로 한 칸씩 이동한다 생각해도 됨
-'''
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        if len(nums) < 3: return max(nums)
         
-        rob1 = 0 #dp[i-1]
-        rob2 = 0 #dp[i-2]
-        
+        arr = [0 for _ in range(len(nums))]
+        arr[0] = nums[0]
+        arr[1] = max(nums[1],nums[0])
 
+        for i in range(2,len(nums)):
+            arr[i] = max(arr[i-1],nums[i] + arr[i-2])
         
-        for n in nums:
-            temp = max(rob1,rob2 + n)
-            rob2 = rob1
-            rob1 = temp
-        return max(rob1,rob2)
+        return arr[-1]
