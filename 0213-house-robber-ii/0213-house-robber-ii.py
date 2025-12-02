c2dc5d844c99e2d8dd59ca3662e648b606e84511
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-12-01 20:48:44 -0500

    Time: 0 ms (100%), Space: 17.6 MB (86.46%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -11,7 +11,7 @@ class Solution:
 
         arr = [0 for _ in range(len(nums))]
         arr[0] = nums[0]
-        arr[1] = nums[1]
+        arr[1] = max(nums[1],nums[0])
 
         for i in range(2,len(nums)):
             arr[i] = max(nums[i] + arr[i-2], arr[i-1])
