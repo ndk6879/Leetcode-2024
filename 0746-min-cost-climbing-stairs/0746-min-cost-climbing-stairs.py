42ab84056a8c76dc958a926c961dca5513bd3d8d
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2025-12-01 20:31:14 -0500

    Time: 7 ms (20.95%), Space: 17.9 MB (62.82%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,9 +1,16 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        n = len(cost)
-        a, b = 0, 0  # a = dp[i-2], b = dp[i-1]
+        
+        # 1. 각 자리 = cost[i] + min(i-1,i-2) 
+        # 2. 각 자리에 올수있는 애들은 i-2,i-1
+        # 3. top = 마지막 index+1의 위치구나
 
-        for c in cost:
-            a, b = b, min(a, b) + c
+        arr = [0 for _ in range(len(cost))]
+        arr[0] = cost[0]
+        arr[1] = cost[1]
 
-        return min(a, b)
+        for i in range(2,len(arr)):
+            arr[i] = cost[i] + min(arr[i-1],arr[i-2])
+        
+        print(arr)
+        return min(arr[-2],arr[-1])
