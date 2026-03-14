1082bedfd85f72ff3f9b6f16ea23a054f82cf0e7
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2026-03-14 03:02:35 -0400

    Time: 13 ms (12.04%), Space: 25 MB (11.23%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,28 +1,28 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # { num : occurrence }
-        # [] * len(nums)
-        # for loop in backwards to find the most freq one
+        
+        '''
+        - 1. create hashMap and count how many each element exists
+        - 2. create Array with len(nums+1) and assign elements from nums at index where each index is key in hashMap
+        -3. count in backwards
+        '''
 
         hashMap = {}
         for num in nums:
             hashMap[num] = hashMap.get(num,0) + 1
+            
+        arr = [[] for _ in range(len(nums)+1)]
 
-        arr = [[] for _ in range(len(nums))]
-
-        for num, occur in hashMap.items():
-            print(num,occur)
-            arr[occur-1].append(num)
-
-
+        for b,v in hashMap.items():
+            arr[v].append(b)
+        
         ans = []
-
+        print(arr)
         for i in range(len(arr)-1,-1,-1):
-            
-            cur = arr[i]
-            
-            while len(cur) > 0:
-                tmp = cur.pop()
-                ans.append(tmp)
-                k -= 1
-                if k == 0: return ans
+
+            while len(arr[i]) > 0:
+                cur = arr[i].pop()
+                ans.append(cur)
+                k -=1 
+                if k == 0:return ans
+
