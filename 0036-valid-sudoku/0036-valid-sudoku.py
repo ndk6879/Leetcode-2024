983bc2f7c3d31571e1d4aa818fe8815be86e7c5e
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2026-03-14 04:01:47 -0400

    Time: 3 ms (76.47%), Space: 19.3 MB (69.26%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -3,27 +3,25 @@ from collections import defaultdict
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         
+        #create row,col,square hashMap 
+        # iterate over every cell in board and check
+        
         row = defaultdict(set)
         col = defaultdict(set)
         square = defaultdict(set)
 
         for r in range(len(board)):
             for c in range(len(board[0])):
-
+                
                 cur = board[r][c]
-
-                if cur == '.': 
+                if cur == '.':
                     continue
 
-                if (cur in row[r]) or (cur in col[c]) or (cur in square[(r//3,c//3)]):
+                if cur in row[r] or cur in col[c] or cur in square[(r//3 , c//3)]:
                     return False
-
                 
-
                 row[r].add(cur)
                 col[c].add(cur)
-                square[(r//3,c//3)].add(cur)
-
+                square[(r//3 , c//3)].add(cur)
         
-
         return True
