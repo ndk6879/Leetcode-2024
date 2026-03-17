d79216baee1bce4c8eb65a0da5eaf9dff37773f4
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2026-03-17 17:57:44 -0400

    Time: 35 ms (7.14%), Space: 19.5 MB (89.72%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,3 +1,5 @@
+#each time, in decision tree, we can make 2 decisions and elment can be 1. So, time complexity = 2^t where t = target
+#그리고 backtracking을 함으로써, [2,2,3]을 먼저 찾아서 [3,2,2] 같은 일이 발생하지 않음
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         
