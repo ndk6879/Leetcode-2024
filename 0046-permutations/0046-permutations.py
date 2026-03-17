c7167fa96c2bb805390b7994c0dcadf7b7a6b572
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2026-03-17 18:50:13 -0400

    Time: 3 ms (30.87%), Space: 19.6 MB (56.28%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,23 +1,22 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        used = [False] * len(nums)
-
-        def dfs(index,path):
-            if len(path) == len(nums):
-                res.append(path[:])
-                return
-
-            for i in range(len(nums)):
-                if not used[i]:
-                    used[i] = True
-                    path.append(nums[i])
-                    dfs(index+1,path)
-                    path.pop()
-                    dfs(index+1,path)
-                    used[i] = False
-
-            
-        dfs(0,[])
-        return res
         
+        ans = []
+        subSet = []
+
+        def dfs():
+
+            if len(subSet) == len(nums):
+                ans.append(subSet.copy())
+                return
+            
+            for num in nums:
+                if num in subSet:
+                    continue
+
+                subSet.append(num)                
+                dfs()
+                subSet.pop()
+        
+        dfs()
+        return ans
