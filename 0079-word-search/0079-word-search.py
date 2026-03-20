1727f3e6beedb4de6caefa5db9af017ffd057d3f
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2026-03-20 02:15:10 -0400

    Time: 4623 ms (24.03%), Space: 19.3 MB (97.65%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,33 +1,39 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         
+        '''
+        1. use 2 for loops to iterate over on ea ch character
+        2. do dfs on each character 
+        3. return False if none of character matches or return true at 
+        '''
 
         visit = set()
-        def dfs(i,j,index):
-            if index == len(word):
-                return True
+        def dfs(cnt,i,j):
 
-            if (i < 0 or 
-            i >= len(board) or
-            j < 0 or 
-            j >= len(board[0]) or
-            board[i][j] != word[index] or
+            if cnt == len(word):
+                return True
+            
+            if (i < 0 or i >= len(board) or
+            j < 0 or j >= len(board[0]) or 
+            board[i][j] != word[cnt] or 
             (i,j) in visit
             ):
                 return False
 
             visit.add((i,j))
 
-            res = (dfs(i+1,j,index+1) or dfs(i,j+1,index+1) or dfs(i-1,j,index+1) or dfs(i,j-1,index+1))
-
-
+            res = (dfs(cnt+1,i-1,j) or dfs(cnt+1,i+1,j) or dfs(cnt+1,i,j-1) or dfs(cnt+1,i,j+1))
             visit.remove((i,j))
 
             return res
 
+            
+
+
+
         for i in range(len(board)):
             for j in range(len(board[0])):
-                if dfs(i,j,0):
+                if dfs(0,i,j):
                     return True
-
+        
         return False
