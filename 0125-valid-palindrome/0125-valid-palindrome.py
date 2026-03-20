a9a970491809ad6b1b63e8a018c2779eaa9473cc
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2026-03-20 03:45:41 -0400

    Time: 11 ms (33.76%), Space: 19.6 MB (72.08%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,19 +1,26 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         
-        l, r = 0, len(s) - 1
+        '''
+        - Use two pointer variables l,r at 0, len(s) - 1 index for each
+        - check if character at eaach index is character. If not, go to next one or change it to lower alphabet
+        - check if they are different and return False
+        - if they meet, just return True 
+        '''
 
+
+        l,r = 0, len(s) - 1
+        
         while (l < r):
-
-            while l < r and not s[l].isalnum():
+            while l < r and  not s[l].isalnum():
                 l += 1
-
-            while l < r and not s[r].isalnum():
+            
+            while l < r and    not s[r].isalnum():
                 r -= 1
             
-            if s[l].lower() == s[r].lower():
-                l += 1
-                r -= 1
-            else:
+            if s[l].lower() != s[r].lower():
                 return False
+            
+            l += 1
+            r -= 1
         return True
