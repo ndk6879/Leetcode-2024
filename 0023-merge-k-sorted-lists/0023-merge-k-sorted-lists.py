0bff6dac3f165e4162bcdc8954d58265b157e512
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2026-03-26 11:02:05 -0400

    Time: 15 ms (29.04%), Space: 22.8 MB (75.12%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -5,27 +5,19 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-
-        #1. merge two sub lists in lists arr
-        #2. update the lists with those two sub lists
-
-        if not lists:
-            return None
-
-
-        while len(lists) > 1:
-            sub = []
-            for i in range(0,len(lists),2):
-                
-                l1 = lists[i]
-                l2 = lists[i+1] if (i + 1) < len(lists) else None
-
-                sub.append(self.mergeLists(l1,l2))
-            lists = sub
-        return lists[0]
-
+        if len(lists) == 0: return None
         
-    def mergeLists(self,l1,l2):
+        while (len(lists) > 1):
+            cur = []
+            for i in range(0,len(lists),2):
+                l1 = lists[i]
+                l2 = lists[i+1] if i+1 < len(lists) else None
+                
+                cur.append(self.mergedLists(l1,l2))
+            lists = cur
+        return lists[0]
+    
+    def mergedLists(self,l1,l2):
 
         ans = cur = ListNode()
 
@@ -33,15 +25,19 @@ class Solution:
             if l1.val <= l2.val:
                 cur.next = l1
                 l1 = l1.next
-
             else:
-                cur.next = l2
+                cur.next = l2 
                 l2 = l2.next
             cur = cur.next
         
-       
-        cur.next = l1 or l2
-        return ans.next
-                
+        while l1:
+            cur.next = l1
+            l1 = l1.next
+            cur = cur.next
         
-
+        while l2:
+            cur.next = l2
+            l2 = l2.next
+            cur = cur.next
+        
+        return ans.next
