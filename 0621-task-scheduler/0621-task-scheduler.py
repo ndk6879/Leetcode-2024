80b6ad6557091858ee88819f1664e0110f146e41
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2026-04-13 20:53:25 -0400

    Time: 311 ms (5.93%), Space: 20.9 MB (46.3%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,26 +1,32 @@
-from collections import Counter, deque
+from collections import Counter
 import heapq
 
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         
+        '''
+        use heap to find max freq -> so we can use the most frequent one first to minize CPU
+        '''
+
         counter = Counter(tasks)
-        maxHeap = [-task for task in counter.values()] # [-3,-3]
-        heapq.heapify(maxHeap)
-        queue = deque([])
-
+        heap = [-task for task in counter.values()] #[-3, -3]
+        print('heap:',heap)
+        heapq.heapify(heap)
+        arr = deque([])
         time = 0
-        while maxHeap or queue:
+
+        while heap or arr:
             time += 1
-            
-            if maxHeap:
-                cur = heapq.heappop(maxHeap)
 
-                if cur + 1 != 0:
-                    queue.append([cur+1, time + n])
+            if heap:
+                cur = heapq.heappop(heap)
+                print('cur:',cur)
 
-            if queue and queue[0][1] == time:
-                cur = queue.popleft()
-                heapq.heappush(maxHeap,cur[0])
-        
+                if cur + 1 != 0 :
+                    arr.append([cur+1, time + n]) #[ [-2,3] ]
+
+            if arr and arr[0][-1] == time:
+                cur = arr.popleft()
+                heapq.heappush(heap,cur[0])
+
         return time
