f4306f9985c47b6d6b388ab4666d24292ec5a181
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2026-04-16 03:20:52 -0400

    Time: 0 ms (100%), Space: 19.5 MB (11.81%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,19 +1,17 @@
-#.copy() = 지금 상태의 리스트를 새로 복사해서 저장하려고 쓰는 것
-
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        
         ans = []
-        subSet = []
-
-        def dfs(ind):
-            if ind >= len(nums):
-                ans.append(subSet.copy())
+        cur = []
+        def dfs(i):
+            if i >= len(nums):
+                ans.append(cur[:])
                 return
             
-            subSet.append(nums[ind])
-            dfs(ind+1)
-            subSet.pop()
-            dfs(ind+1)
+            cur.append(nums[i])
+            dfs(i+1)
+            cur.pop()
+            dfs(i+1)
 
         dfs(0)
         return ans
