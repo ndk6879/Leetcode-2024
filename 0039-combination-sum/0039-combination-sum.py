f19da467c5190788be1b2fd8504a30926da356dc
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2026-04-16 03:42:05 -0400

    Time: 9 ms (53.37%), Space: 19.7 MB (15.35%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,26 +1,21 @@
-'''
-현재 숫자를 쓴다
-현재 숫자를 안 쓰고 다음 숫자로 간다
-'''
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         
         ans = []
-        subSet = []
-
-        def dfs(i):
-            if sum(subSet[:]) == target:
-                ans.append(subSet[:])
+        cur = []
+        def dfs(i, total):
+            if total == target:
+                ans.append(cur[:])
                 return
             
-            if i >= len(candidates) or sum(subSet[:]) > target:
+            if i >= len(candidates) or total > target:
                 return
             
+            cur.append(candidates[i])
+            dfs(i,total + candidates[i])
+            cur.pop()
 
-            subSet.append(candidates[i])
-            dfs(i)
-            subSet.pop()
-            dfs(i+1)
+            dfs(i+1,total)
 
-        dfs(0)
+        dfs(0,0)
         return ans
