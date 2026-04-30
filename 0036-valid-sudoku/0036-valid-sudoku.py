7f608d0e30668609b2e4098f7c4c56a3c3075b97
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2026-04-30 13:29:04 -0400

    Time: 7 ms (30.14%), Space: 19.3 MB (74.98%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -9,13 +9,18 @@ class Solution:
 
         for r in range(len(board)):
             for c in range(len(board[0])):
+
                 cur = board[r][c]
                 if cur == '.': continue
+
                 if cur in row[r] or cur in col[c] or cur in square[(r//3,c//3)]:
                     return False
+
+                
 
                 row[r].add(cur)
                 col[c].add(cur)
                 square[(r//3,c//3)].add(cur)
+        
+    
         return True
-                
