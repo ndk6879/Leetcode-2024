fff8d91148970d49f8b8b8ad78326fc0f266c1f8
Andy Nam (Donggyun) <46705205+ndk6879@users.noreply.github.com>
2026-05-14 16:26:02 -0400

    Time: 671 ms (32.55%), Space: 22 MB (93.66%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,32 +1,27 @@
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
         
-        '''
-        1. sort array
-        2. use two pointers l,r to check if the sum of window is 0
-        '''
-
-        nums.sort()
         ans = []
+        nums.sort()
         for l in range(len(nums)-2):
+            if l > 0 and nums[l] == nums[l-1]: continue
+            
             i = l + 1
             r = len(nums) - 1
-            if l > 0 and nums[l] == nums[l - 1]:
-                continue
-            
             while (i < r):
                 total = nums[l] + nums[i] + nums[r]
-
+                
                 if total == 0:
                     ans.append([nums[l] , nums[i] , nums[r]])
                     i += 1
-
-                    while (i < r and nums[r] == nums[r-1]):
+                    while i < r and nums[i] == nums[i-1]:
                         i += 1
                 
                 elif total < 0:
                     i += 1
-
+                
                 else:
                     r -= 1
+            
+        
         return ans
